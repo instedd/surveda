5b0a668abe353053fac0a1ec22703c93ec178bf6
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-08-12 12:24:34 -0300

    Add User model

---
+++ web/models/user.ex
@@ -0,0 +1,19 @@
+defmodule Ask.User do
+  use Ask.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :encrypted_password, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :encrypted_password])
+    |> validate_required([:email, :encrypted_password])
+  end
+end
