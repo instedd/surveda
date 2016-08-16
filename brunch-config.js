9316daa0d135844cc68cced494f0367f8b08adf9
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-08-16 14:39:58 -0300

    Add React + redux

--- brunch-config.js
+++ brunch-config.js
@@ -52,6 +52,8 @@ exports.config = {
   // Configure your plugins
   plugins: {
     babel: {
+      presets: ['es2015', 'react', 'stage-2', 'stage-0'],
+
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
     }
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,7 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
+ReactDOM.render(
+  <h1>Welcome to Ask!</h1>,
+  document.getElementById('main_container')
+);
