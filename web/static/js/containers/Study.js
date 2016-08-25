5d013ad9f1f61a09834b072a76beccd630b8361c
Mariano Abel Coca <marianoabelcoca@gmail.com>
2016-08-25 14:34:01 -0300

    Fetching study on page reload

--- web/static/js/containers/Study.js
+++ web/static/js/containers/Study.js
@@ -2,16 +2,30 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 //import { v4 } from 'node-uuid'
 import { Link, withRouter } from 'react-router'
+import * as actions from '../actions'
+import { fetchStudies } from '../api'
 
 class Study extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props
+    fetchStudies().then(studies => dispatch(actions.fetchStudiesSuccess(studies)))
+  }
+
+  componentDidUpdate() {
+  }
+
   render(params) {
     const { study } = this.props
-    return (
-      <div>
-        <h1>{ study.name }</h1>
-        <Link to='/studies'>Back</Link>
-      </div>
-    )
+    if(study) {
+      return (
+        <div>
+          <h1>{ study.name }</h1>
+          <Link to='/studies'>Back</Link>
+        </div>
+      )
+    } else {
+      return <p>Loading...</p>
+    }
   }
 }
 
