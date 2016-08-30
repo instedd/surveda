829cd16ceafec741a7dfae6566af8d28e8edabf6
Juan F. Blandini <jblandini@manas.com.ar>
2016-08-30 15:17:53 -0300

    Changing create study migration to reflect the new model and start properly the database on new installations

--- priv/repo/migrations/20160816183915_create_project.exs
+++ priv/repo/migrations/20160816183915_create_project.exs
@@ -1,14 +1,14 @@
-defmodule Ask.Repo.Migrations.CreateStudy do
+defmodule Ask.Repo.Migrations.CreateProject do
   use Ecto.Migration
 
   def change do
-    create table(:studies) do
+    create table(:projects) do
       add :name, :string
       add :user_id, references(:users, on_delete: :nothing)
 
       timestamps()
     end
-    create index(:studies, [:user_id])
+    create index(:projects, [:user_id])
 
   end
 end
