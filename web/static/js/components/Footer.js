945685546bbedc94be3008e6a708f5fd445adb03
Juan F. Blandini <jblandini@manas.com.ar>
2016-08-30 11:13:22 -0300

    Small adjustment of the footer

--- web/static/js/components/Footer.js
+++ web/static/js/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default () => (
-  <div style={{position: 'absolute', bottom: 0, height: '40px', width: '50%'}}>
+  <div style={{position: 'absolute', bottom: 0, height: '50px', width: '50%'}}>
     <div style={{clear: 'both', display: 'block', borderTop: '1px solid #000000'}}></div>
     <p>This is the Footer</p>
     <p>© 2016 InSTEDD </p>
