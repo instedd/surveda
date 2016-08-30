360776564592d5982012be67d7f0e32b7a540724
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-08-30 18:43:54 -0300

    Added missing dependency for material-ui (react-tap-event-plugin)

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -5,10 +5,12 @@ import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 const store = configureStore()
 const history = syncHistoryWithStore(browserHistory, store)
+injectTapEventPlugin()
 
 render(
   <MuiThemeProvider>
