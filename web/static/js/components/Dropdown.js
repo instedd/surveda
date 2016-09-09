a5a26258a527a4c7d77a813137ec6a32cb65c36a
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-09-09 11:45:34 -0300

    Suppress default event from Dropdown button (so it doesn't navigate to `#!`)
    Signed-off-by: Juan Wajnerman <jwajnerman@manas.com.ar>

--- web/static/js/components/Dropdown.js
+++ web/static/js/components/Dropdown.js
@@ -3,10 +3,13 @@ import { uniqueId } from 'lodash'
 
 export const Dropdown = ({text, icon = 'arrow_drop_down', children}) => {
   const dropdownId = uniqueId('dropdown')
+  const onButtonClick = (event) => {
+    event.preventDefault()
+  }
 
   return (
     <div>
-      <a className="dropdown-button" href="#!" data-induration="100" data-outduration="50" data-beloworigin="true" data-activates={dropdownId}>
+      <a className="dropdown-button" href="#!" onClick={onButtonClick} data-induration="100" data-outduration="50" data-beloworigin="true" data-activates={dropdownId}>
         {text}
         <i className="material-icons right">{icon}</i>
       </a>
