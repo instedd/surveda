d7b0782fda2e787002c7af5d20762ca6e0604843
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-09-09 00:11:06 -0300

    Fix initially selected tab

--- web/static/js/components/TabLink.js
+++ web/static/js/components/TabLink.js
@@ -2,20 +2,15 @@ import React from 'react'
 import { Link, withRouter } from 'react-router'
 
 export const TabLink = withRouter(props => {
-  const {children, to, router, params, location, routes, ...attrs} = props;
-
-  attrs.className = ""
-  if (router.isActive(to)) {
-    attrs.className = "active"
-  }
-  attrs.className += " tab col"
+  const { children, to, router } = props;
+  const className = router.isActive(to) ? "active" : ""
   const clickHandler = () => {
     router.push(to)
   }
 
   return (
-    <li {...attrs}>
-      <a onClick={clickHandler}>{children}</a>
+    <li className="tab col">
+      <a onClick={clickHandler} className={className}>{children}</a>
     </li>
   )
 })
