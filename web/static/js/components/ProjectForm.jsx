dce14c964c0a52334496ab906d9a9d9df980cb16
Mariano Abel Coca <marianoabelcoca@gmail.com>
2016-09-22 12:19:16 -0300

    Cleaned up ProjectForm input implementation.

--- web/static/js/components/ProjectForm.jsx
+++ web/static/js/components/ProjectForm.jsx
@@ -1,33 +1,35 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import merge from 'lodash/merge'
 import { Link } from 'react-router'
 
-const ProjectForm = ({ onSubmit, project }) => {
-  let input
-  if (!project) {
-    return <div>Loading...</div>
-  }
+class ProjectForm extends Component {
+  render() {
+    const { onSubmit, project } = this.props
 
-  return (
-    <div>
+    if (!project) {
+      return <div>Loading...</div>
+    }
+
+    return (
       <div>
-        <label>Project Name</label>
         <div>
-          <input type="text" placeholder="Project name" defaultValue={project.name} ref={ node => { input = node }
-          }/>
+          <label>Project Name</label>
+          <div>
+            <input type="text" placeholder="Project name" defaultValue={project.name} ref="input"/>
+          </div>
+        </div>
+        <br/>
+        <div>
+          <button type="button" onClick={() =>
+            onSubmit(merge({}, project, { name: this.refs.input.value }))
+          }>
+            Submit
+          </button>
+          <Link to='/projects'> Back</Link>
         </div>
       </div>
-      <br/>
-      <div>
-        <button type="button" onClick={() =>
-          onSubmit(merge({}, project, {name: input.value}))
-        }>
-          Submit
-        </button>
-        <Link to='/projects'> Back</Link>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 ProjectForm.propTypes = {
