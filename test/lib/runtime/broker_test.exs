4a3b9f3b3ac0e1a8d4b3747f79123652f2fb52b4
Juan F. Blandini <jblandini@manas.com.ar>
2016-09-26 15:59:55 -0300

    Adding tests to broker, and some refactors to share initializations between tests. Related with #57: Broker must stop when reaching the cutoff rule

--- test/lib/runtime/broker_test.exs
+++ test/lib/runtime/broker_test.exs
@@ -30,15 +30,52 @@ defmodule Ask.BrokerTest do
     assert survey.state == "running"
   end
 
+  test "changes the respondent state from pending to running if neccessary" do
+    [survey, _, respondent, _] = create_survey_with_channel_and_respondent()
+
+    Broker.handle_info(:poll, nil)
+
+    survey = Repo.get(Survey, survey.id)
+    assert survey.state == "running"
+    updated_respondent = Repo.get(Respondent, respondent.id)
+    assert updated_respondent.state == "active"
+  end
+
+  test "keeps batch_size respondents running" do
+    [survey, _, _, _] = create_survey_with_channel_and_respondent()
+    create_several_respondents(survey, 20)
+
+    Broker.handle_info(:poll, nil)
+
+    survey = Repo.get(Survey, survey.id)
+
+    assert survey.state == "running"
+
+    [active, pending] = get_respondents_by_state(survey)
+
+    assert active == 10
+    assert pending == 11
+
+    active_respondent = Repo.all(from r in Respondent, where: r.state == "active")
+    |> Enum.at(0)
+
+    Repo.update(active_respondent |> change |> Respondent.changeset(%{state: "failed"}))
+
+    [active, pending] = get_respondents_by_state(survey)
+
+    assert active == 9
+    assert pending == 11
+
+    Broker.handle_info(:poll, nil)
+
+    [active, pending] = get_respondents_by_state(survey)
+
+    assert active == 10
+    assert pending == 10
+  end
+
   test "respondent flow" do
-    test_channel = TestChannel.new
-    channel = insert(:channel, settings: test_channel |> TestChannel.settings)
-    quiz = insert(:questionnaire, steps: @dummy_steps)
-    survey = insert(:survey, state: "running", questionnaire: quiz) |> Repo.preload([:channels])
-    channel_changeset = Ecto.Changeset.change(channel)
-    survey |> Ecto.Changeset.change |> Ecto.Changeset.put_assoc(:channels, [channel_changeset]) |> Repo.update
-    respondent = insert(:respondent, survey: survey)
-    phone_number = respondent.phone_number
+    [survey, test_channel, respondent, phone_number] = create_survey_with_channel_and_respondent()
 
     {:ok, broker} = Broker.start_link
     broker |> send(:poll)
@@ -61,5 +98,34 @@ defmodule Ask.BrokerTest do
     respondent = Repo.get(Respondent, respondent.id)
     assert respondent.state == "completed"
     assert respondent.session == nil
+  end
+
+  def create_survey_with_channel_and_respondent() do
+    test_channel = TestChannel.new
+    channel = insert(:channel, settings: test_channel |> TestChannel.settings)
+    quiz = insert(:questionnaire, steps: @dummy_steps)
+    survey = insert(:survey, state: "running", questionnaire: quiz) |> Repo.preload([:channels])
+    channel_changeset = Ecto.Changeset.change(channel)
+    survey |> Ecto.Changeset.change |> Ecto.Changeset.put_assoc(:channels, [channel_changeset]) |> Repo.update
+    respondent = insert(:respondent, survey: survey)
+    phone_number = respondent.phone_number
+
+    [survey, test_channel, respondent, phone_number]
+  end
+
+  def create_several_respondents(survey, n) when n <= 1 do
+    [insert(:respondent, survey: survey)]
+  end
+
+  def create_several_respondents(survey, n) do
+    [create_several_respondents(survey, n - 1) | insert(:respondent, survey: survey)]
+  end
+
+  def get_respondents_by_state(survey) do
+    by_state = Repo.all(
+      from r in assoc(survey, :respondents),
+      group_by: :state,
+      select: {r.state, count("*")}) |> Enum.into(%{})
+    [by_state["active"] || 0, by_state["pending"] || 0]
   end
 end
