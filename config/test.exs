5f850dbdb7e153d1b2d3d5fb0b26094b3ca4a041
Martin Verzilli <mverzilli@manas.com.ar>
2016-09-26 17:47:35 -0300

    Expose Verboice Guisso config params to the client (related to #62)

--- config/test.exs
+++ config/test.exs
@@ -25,3 +25,19 @@ config :ask, :channel,
   providers: %{
     "test" => Ask.TestChannel
   }
+
+config :ask, Nuntium,
+  base_url: "http://nuntium.com",
+  guisso: [
+    base_url: "http://nuntium.com",
+    client_id: "A_NO_SO_SECRET_CLIENT_ID",
+    app_id: "AN_APP_ID"
+  ]
+
+config :ask, Verboice,
+  base_url: "http://verboice.com",
+  guisso: [
+    base_url: "http://verboice.com",
+    client_id: "A_NO_SO_SECRET_CLIENT_ID",
+    app_id: "AN_APP_ID"
+  ]
