7bb84e5db5b888ce787d085b6bdcb399472c7171
Martin Verzilli <mverzilli@manas.com.ar>
2016-10-05 17:40:06 -0300

    WIP #70: simplifying reducer

--- web/static/js/reducers/questionnaireEditor.js
+++ web/static/js/reducers/questionnaireEditor.js
@@ -1,4 +1,5 @@
 import * as actions from '../actions/questionnaireEditor'
+import reduce from 'lodash/reduce'
 
 const defaultState = {
   steps: {
@@ -21,19 +22,6 @@ export default (state = defaultState, action) => {
         currentStepId: null
       }
     case actions.INITIALIZE_EDITOR:
-      var stepsItems = {}
-      action.questionnaire.steps.map((step) =>{
-        var responses = {}
-        if(step.choices){
-          responses['items'] = step.choices.map((choice) =>{
-            var responseItem = {}
-            responseItem["response"] = choice.value
-            return responseItem
-          })
-        }
-        // The id isn't in the proposed model, but it's necessary for the view
-        stepsItems[step.id] = {title: step.title, responses: responses, id: step.id}
-      })
       return {
         ...state,
         questionnaire: {
@@ -41,13 +29,24 @@ export default (state = defaultState, action) => {
           name: action.questionnaire.name
         },
         steps: {
-          ids: action.questionnaire.steps.map((step) =>{
-            return step.id
-          }),
-          items: stepsItems
+          ids: action.questionnaire.steps.map(step => step.id),
+          items: reduce(action.questionnaire.steps, mapStepModel, {})
         }
       }
     default:
       return state
   }
+}
+
+const mapStepModel = (items, currentStep) => {
+  let responses = {}
+  if (currentStep.choices) {
+    responses['items'] = currentStep.choices.map((choice) => {
+      var responseItem = {}
+      responseItem['response'] = choice.value
+      return responseItem
+    })
+  }
+  items[currentStep.id] = {title: currentStep.title, responses: responses, id: currentStep.id}
+  return items
 }
