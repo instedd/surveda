6b9c4349870432e31c11dc4a22cf82a44ba676da
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-10-13 13:54:25 -0300

    Set default value for existing respondents in 'completed' state

---
+++ priv/repo/migrations/20161013162339_set_default_completed_at_for_completed_respondents.exs
@@ -0,0 +1,7 @@
+defmodule Ask.Repo.Migrations.SetDefaultCompletedAtForCompletedRespondents do
+  use Ecto.Migration
+
+  def change do
+    Ask.Repo.query("UPDATE respondents SET completed_at = updated_at WHERE state = 'completed' AND completed_at IS NULL")
+  end
+end
