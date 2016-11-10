9774c33a300802a75fb69f980d33c8afc3b1f37a
Fernando Gasperi <fgasperijabalera@gmail.com>
2016-11-10 13:09:00 -0300

    (#175) Added file size validation.

--- web/models/audio.ex
+++ web/models/audio.ex
@@ -18,6 +18,7 @@ defmodule Ask.Audio do
     struct
     |> cast(params, [:data, :filename])
     |> validate_change(:filename, &validate_file_type/2)
+    |> validate_change(:data, &validate_size/2)
   end
 
   def validate_file_type(:filename, filename) do
@@ -30,6 +31,17 @@ defmodule Ask.Audio do
     else
       [filename: "Invalid file type. Allowed types are MPEG and WAV."]
     end
+  end
+
+  def validate_size(:data, data) do
+    size = byte_size(data)
+    mb_size = size/:math.pow(2, 20)
+    if mb_size < 16 do
+      []
+    else
+      [data: "The file is too big, please do not exceed 16MB."]
+    end
+
   end
 
 end
