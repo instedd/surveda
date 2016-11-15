03d99089fe1dbf0dc9d0f8c74632bf089c1bd69a
Martin Verzilli <mverzilli@manas.com.ar>
2016-11-15 11:43:36 -0300

    Refactor Verboice channel tests
    Now there's a map of possible steps to expected TwiML output, so it's easier to test different combinations

--- test/lib/runtime/verboice_channel_test.exs
+++ test/lib/runtime/verboice_channel_test.exs
@@ -20,49 +20,34 @@ defmodule Ask.Runtime.VerboiceChannelTest do
   setup %{conn: conn} do
     GenServer.start_link(BrokerStub, [], name: Broker.server_ref)
     respondent = insert(:respondent, phone_number: "123", state: "active")
-    {:ok, conn: conn, respondent: respondent}
+    {
+      :ok, 
+      conn: conn, 
+      respondent: respondent,
+      tts_step: {:prompt, Ask.StepBuilder.tts_prompt("Do you exercise?")},
+      # Triple of "digits", step spec, and expected TwiML output
+      twiml_map: [
+        {nil, {:prompt, Ask.StepBuilder.tts_prompt("Do you exercise?")},                      "<Say>Do you exercise?</Say>"},
+        {"8", {:prompt, Ask.StepBuilder.tts_prompt("Do you exercise?")},                      "<Say>Do you exercise?</Say>"},
+        {nil, :end,                                                                           "<Response><Hangup/></Response>"},
+        {nil, {:prompt, Ask.StepBuilder.audio_prompt(uuid: "foo", text: "Do you exercise?")}, "<Play>http://app.ask.dev/audio/foo</Play>"},
+        {"8", {:prompt, Ask.StepBuilder.audio_prompt(uuid: "foo", text: "Do you exercise?")}, "<Play>http://app.ask.dev/audio/foo</Play>"},
+      ]
+    }
   end
 
-  test "callback with :prompt", %{conn: conn, respondent: respondent} do
+  test "callbacks", %{conn: conn, respondent: respondent, twiml_map: twiml_map} do
     respondent_id = respondent.id
-    GenServer.cast(Broker.server_ref, {:expects, fn
-      {:sync_step, %Respondent{id: ^respondent_id}, nil} ->
-        {:prompt, Ask.StepBuilder.tts_prompt("Do you exercise?")}
-    end})
 
-    conn = VerboiceChannel.callback(conn, %{"respondent" => respondent_id})
-    assert response(conn, 200) =~ "<Say>Do you exercise?</Say>"
-  end
+    Enum.each(twiml_map, fn 
+      {digits, step, twiml} -> 
+        GenServer.cast(Broker.server_ref, {:expects, fn
+          {:sync_step, %Respondent{id: ^respondent_id}, digits} -> step
+        end})
 
-  test "callback with digits", %{conn: conn, respondent: respondent} do
-    respondent_id = respondent.id
-    GenServer.cast(Broker.server_ref, {:expects, fn
-      {:sync_step, %Respondent{id: ^respondent_id}, "8"} ->
-        {:prompt, Ask.StepBuilder.tts_prompt("Do you exercise?")}
-    end})
-    conn = VerboiceChannel.callback(conn, %{"respondent" => respondent_id, "Digits" => "8"})
-    assert response(conn, 200) =~ "<Say>Do you exercise?</Say>"
-  end
-
-  test "callback with :end", %{conn: conn, respondent: respondent} do
-    respondent_id = respondent.id
-    GenServer.cast(Broker.server_ref, {:expects, fn
-      {:sync_step, %Respondent{id: ^respondent_id}, nil} ->
-        :end
-    end})
-    conn = VerboiceChannel.callback(conn, %{"respondent" => respondent_id})
-    assert response(conn, 200) == "<Response><Hangup/></Response>"
-  end
-
-  test "callback with audio", %{conn: conn, respondent: respondent} do
-    respondent_id = respondent.id
-    GenServer.cast(Broker.server_ref, {:expects, fn
-      {:sync_step, %Respondent{id: ^respondent_id}, nil} ->
-        { :prompt, Ask.StepBuilder.audio_prompt(uuid: "foo", text: "Do you exercise?") }
-    end})
-
-    conn = VerboiceChannel.callback(conn, %{"respondent" => respondent_id})
-    assert response(conn, 200) =~ "<Play>http://app.ask.dev/audio/foo</Play>"
+        conn = VerboiceChannel.callback(conn, %{"respondent" => respondent_id, "Digits" => digits})
+        assert response(conn, 200) =~ twiml
+      end)
   end
 
   test "callback respondent not found", %{conn: conn} do
