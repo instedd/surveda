543495a9abfead82fb632c6738fab088d9f8df27
Martin Scebba <mscebba@manas.com.ar>
2016-12-06 21:43:08 -0300

    Jump page removed when selecting new language on questionnaire

--- web/static/js/components/questionnaires/AddLanguage.jsx
+++ web/static/js/components/questionnaires/AddLanguage.jsx
@@ -41,7 +41,7 @@ class AddLanguage extends Component {
   render() {
     if (!this.state.editing) {
       return (
-        <a className='btn-icon-grey' href='#' onClick={this.handleClick}>
+        <a className='add-language' onClick={this.handleClick}>
           <i className='material-icons'>add</i>
           <span>Add Language</span>
         </a>
