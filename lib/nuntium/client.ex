183b0b5700950644c1511ffacbc2e1f402b92341
Juan Wajnerman <jwajnerman@manas.com.ar>
2016-12-16 19:24:34 -0300

    Encode parameters in Nuntium client (fixes issues with accounts names with special characters)

--- lib/nuntium/client.ex
+++ lib/nuntium/client.ex
@@ -12,21 +12,21 @@ defmodule Nuntium.Client do
 
   # @spec send_ao(t, String.t) :: any()
   def send_ao(client, account, messages) do
-    url = "#{client.base_url}/api/ao_messages.json?account=#{account}"
+    url = "#{client.base_url}/api/ao_messages.json?#{URI.encode_query([account: account])}"
     client.oauth2_client
     |> OAuth2.Client.post(url, messages)
     |> parse_response
   end
 
   def application_update(client, account, app = %{}) do
-    url = "#{client.base_url}/api/applications/me?account=#{account}"
+    url = "#{client.base_url}/api/applications/me?#{URI.encode_query([account: account])}"
     client.oauth2_client
     |> OAuth2.Client.put(url, app)
     |> parse_response
   end
 
   def channel_update(client, account, channel_name, settings = %{}) do
-    url = "#{client.base_url}/api/channels/#{channel_name}.json?account=#{account}&application=-"
+    url = "#{client.base_url}/api/channels/#{channel_name}.json?#{URI.encode_query([account: account, application: "-"])}"
     client.oauth2_client
     |> OAuth2.Client.put(url, settings)
     |> parse_response
@@ -40,7 +40,7 @@ defmodule Nuntium.Client do
   end
 
   def get_channels(client, account) do
-    url = "#{client.base_url}/api/channels.json?account=#{account}"
+    url = "#{client.base_url}/api/channels.json?#{URI.encode_query([account: account])}"
     client.oauth2_client
     |> OAuth2.Client.get(url)
     |> parse_response
