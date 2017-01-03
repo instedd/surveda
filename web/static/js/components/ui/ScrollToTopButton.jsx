5d3d231882a8fc834146b1e0f2a271a5cc8d0a9a
Ary Borenszweig <aborenszweig@manas.com.ar>
2017-01-03 16:12:35 -0300

    Fixed flow errors

--- web/static/js/components/ui/ScrollToTopButton.jsx
+++ web/static/js/components/ui/ScrollToTopButton.jsx
@@ -2,43 +2,35 @@
 import React, { Component } from 'react'
 
 export class ScrollToTopButton extends Component {
-
   componentDidMount() {
     $(document).ready(function() {
-      $(window).scroll(function () {
+      $(window).scroll(function() {
         if ($(this).scrollTop() > 500) {
-            $('.scrollToTop').fadeIn()
+          $('.scrollToTop').fadeIn()
         } else {
-            $('.scrollToTop').fadeOut()
+          $('.scrollToTop').fadeOut()
         }
       })
     })
   }
 
-  constructor(props){
-    super(props)
-
-    this.topScrollTo = this.topScrollTo.bind(this)
-  }
-
-  topScrollTo(e) {
+  topScrollTo(e: Event) {
     e.preventDefault()
     scrollToTop()
   }
 
   render() {
-    return <a href='' title='Back to top' className='scrollToTop' onClick={this.topScrollTo}>
-    <i className='material-icons'>arrow_upward</i>
+    return <a href='' title='Back to top' className='scrollToTop' onClick={e => this.topScrollTo(e)}>
+      <i className='material-icons'>arrow_upward</i>
     </a>
   }
 }
 
-
 const scrollToTop = () => {
-  if(window.scrollY>0) {
+  if (window.scrollY > 0) {
     setTimeout(function() {
-       window.scrollTo(0,window.scrollY-35)
-        scrollToTop()
+      window.scrollTo(0, window.scrollY - 35)
+      scrollToTop()
     }, 0)
   }
 }
