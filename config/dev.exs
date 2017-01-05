af31845fe65a19c32e08a12ef2fd3b2868da3cd3
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-01-05 15:08:45 -0300

    Use port 80 when running in development mode in Docker (makes it easier to connect from other containers)

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :ask, Ask.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("HTTP_PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false
