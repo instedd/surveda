3544bf6b01356da727d93fea3abb06f4eecdb0eb
Juan F. Blandini <jblandini@manas.com.ar>
2017-01-06 15:24:11 -0300

    Fixing compilation error on LanguagesList.jsx

--- web/static/js/components/questionnaires/LanguagesList.jsx
+++ web/static/js/components/questionnaires/LanguagesList.jsx
@@ -13,7 +13,7 @@ type Props = {
   activeLanguage: string,
   languages: string[],
   loading: boolean,
-  langHasErrors: Function,
+  langHasErrors: (lang: string) => boolean,
   onRemoveLanguage: Function,
   dispatch: Function
 };
