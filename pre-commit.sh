336792bdb9d671c02e4d8f2571f6224612da7e06
Juan F. Blandini <jblandini@manas.com.ar>
2017-01-23 14:51:32 -0300

    Modifying the small script to run all the tests and checks before commiting, to only show a small message in case of passing, or show all the error output in case of a failure

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
-docker-compose run --rm app mix test
-docker-compose run --rm brunch npm test
-flow check
-docker-compose run --rm brunch node_modules/.bin/eslint --ext .jsx,.js web/static/js/
+
+MIX_TESTS="$(docker-compose run --rm app mix test)"
+
+if [ $? -eq 0 ]; then
+   echo "Mix tests are OK";
+else
+  echo "${MIX_TESTS}"
+fi
+
+BRUNCH_TESTS="$(docker-compose run --rm brunch npm test)"
+
+if [ $? -eq 0 ]; then
+  echo "Brunch tests are OK";
+else
+  echo "${BRUNCH_TESTS}"
+fi
+
+FLOW_TESTS="$(flow check)"
+
+if [ $? -eq 0 ]; then
+  echo "Flow tests are OK";
+else
+  echo "${FLOW_TESTS}"
+fi
+
+ESLINT_TESTS="$(docker-compose run --rm brunch node_modules/.bin/eslint --ext .jsx,.js web/static/js/ test/js)"
+
+if [ $? -eq 0 ]; then
+  echo "Eslint tests are OK";
+else
+  echo "${ESLINT_TESTS}"
+fi
