1bcdc28fb6afc562f83b808baeb8799c8eb4a272
Juan F. Blandini <jblandini@manas.com.ar>
2017-01-25 16:16:29 -0300

    Change on the pre-commit script output messages

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,33 +1,36 @@
 #!/bin/sh
-
+echo "Running Mix tests"
 MIX_TESTS="$(docker-compose run --rm app mix test)"
 
 if [ $? -eq 0 ]; then
-   echo "Mix tests are OK";
+   echo "OK";
 else
   echo "${MIX_TESTS}"
 fi
 
+echo "Running Brunch tests"
 BRUNCH_TESTS="$(docker-compose run --rm brunch npm test)"
 
 if [ $? -eq 0 ]; then
-  echo "Brunch tests are OK";
+  echo "OK";
 else
   echo "${BRUNCH_TESTS}"
 fi
 
+echo "Running Flow tests"
 FLOW_TESTS="$(flow check)"
 
 if [ $? -eq 0 ]; then
-  echo "Flow tests are OK";
+  echo "OK";
 else
   echo "${FLOW_TESTS}"
 fi
 
+echo "Running Eslint tests"
 ESLINT_TESTS="$(docker-compose run --rm brunch node_modules/.bin/eslint --ext .jsx,.js web/static/js/ test/js)"
 
 if [ $? -eq 0 ]; then
-  echo "Eslint tests are OK";
+  echo "OK";
 else
   echo "${ESLINT_TESTS}"
 fi
