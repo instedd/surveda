69eb2511dd6ce8471232feffb55334c88387f2a7
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-02-03 14:24:53 -0300

    Added gen_smtp dependency required for sending emails in prod mode

--- mix.exs
+++ mix.exs
@@ -52,7 +52,8 @@ defmodule Ask.Mixfile do
       {:ex_json_schema, "~> 0.5.2"},
       {:mailgun, git: "https://github.com/chrismccord/mailgun.git", override: true},
       {:appsignal, "~> 0.9.2"},
-      {:coherence, git: "https://github.com/manastech/coherence.git", branch: "v0.3.2"}
+      {:coherence, git: "https://github.com/manastech/coherence.git", branch: "v0.3.2"},
+      {:gen_smtp, "~> 0.11"}
    ]
   end
 
