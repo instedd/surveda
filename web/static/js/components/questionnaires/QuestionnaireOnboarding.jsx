9d302bd4bc14a2849027d59cdf5842870aca4739
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2017-02-07 18:33:46 -0300

    For #491. Add carousel for questionnaire onboarding

--- web/static/js/components/questionnaires/QuestionnaireOnboarding.jsx
+++ web/static/js/components/questionnaires/QuestionnaireOnboarding.jsx
@@ -22,29 +22,31 @@ class QuestionnaireOnboarding extends Component {
       })
   }
 
+  componentDidMount() {
+    $('.carousel.carousel-slider').carousel({
+      full_width: true
+    })
+  }
+
   render() {
     return (
-      <div>
-        <div>
-          <h2>Multi-language questionnaires</h2>
-          <div>
-            When you add more than one language a step for language selection will be enabled and you'll be able to define every step in each of the selected languages.
+      <div className='col s12 m8 offset-m1'>
+        <div className='carousel carousel-slider center' data-indicators='true'>
+          <div className='carousel-fixed-item center'>
+            <a onClick={e => this.hideOnboarding(e)}>Understood, create the first step</a>
           </div>
-        </div>
-        <div>
-          <h2>Questionnaire modes</h2>
-          <div>
-            When more than one mode is enabled you'll be able to set up messages and interactions at every step for each selected mode.
+          <div className='carousel-item' href='#one!'>
+            <h2>Multi-language questionnaires</h2>
+            <p className=''>When you add more than one language a step for language selection will be enabled and you'll be able to define every step in each of the selected languages.</p>
           </div>
-        </div>
-        <div>
-          <h2>Questionnaire steps</h2>
-          <div>
-            Questionnaires are organized in steps. Each step type offers different features like multiple choice, numeric input or just an explanation. The comparison steps allows A/B testing.
+          <div className='carousel-item' href='#two!'>
+            <h2>Questionnaire modes</h2>
+            <p className=''>When more than one mode is enabled you'll be able to set up messages and interactions at every step for each selected mode.</p>
           </div>
-        </div>
-        <div>
-          <a onClick={e => this.hideOnboarding(e)}>Understood, create the first step</a>
+          <div className='carousel-item' href='#three!'>
+            <h2>Questionnaire steps</h2>
+            <p className=''>Questionnaires are organized in steps. Each step type offers different features like multiple choice, numeric input or just an explanation . The comparison steps allow A/B testing.</p>
+          </div>
         </div>
       </div>
     )
