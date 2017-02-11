bbe8a6f92bf70793625b65cee73cdc181ca73658
Martin Scebba <mscebba@manas.com.ar>
2017-02-10 22:07:46 -0300

    Fixes #643 - [Quiz] Apply style to questionnaire onboarding

--- web/static/js/components/questionnaires/QuestionnaireOnboarding.jsx
+++ web/static/js/components/questionnaires/QuestionnaireOnboarding.jsx
@@ -26,34 +26,35 @@ class QuestionnaireOnboarding extends Component {
   render() {
     const { onDismiss } = this.props
     return (
-      <div className='col s12 m8 offset-m1'>
+      <div className='col s12 m5 offset-m2'>
         <div className='carousel carousel-slider center' data-indicators='true'>
           <div className='carousel-fixed-item center'>
-            <a onClick={() => onDismiss()}>Understood, create the first step</a>
+            <a className='btn btn-flat-link' onClick={() => onDismiss()}>Understood, create the first step</a>
           </div>
-          <div className='carousel-item' href='#one!'>
+          <div className='carousel-item grey-text' href='#one!'>
             <div className='icons'>
-              <i className='material-icons'>flag</i>
+              <i className='material-icons large'>flag</i>
             </div>
             <h2>Multi-language questionnaires</h2>
-            <p className=''>When you add more than one language a step for language selection will be enabled and you'll be able to define every step in each of the selected languages.</p>
+            <p>When you add more than one language a step for language selection will be enabled and you'll be able to define every step in each of the selected languages.</p>
           </div>
-          <div className='carousel-item' href='#two!'>
+          <div className='carousel-item grey-text' href='#two!'>
             <div className='icons'>
-              <i className='material-icons'>phone</i>
-              <i className='material-icons'>chat</i>
+              <i className='material-icons large'>textsms</i>
+              <i className='material-icons large'>phone</i>
             </div>
             <h2>Questionnaire modes</h2>
-            <p className=''>When more than one mode is enabled you'll be able to set up messages and interactions at every step for each selected mode.</p>
+            <p>When more than one mode is enabled you'll be able to set up messages and interactions at every step for each selected mode.</p>
           </div>
-          <div className='carousel-item' href='#three!'>
+          <div className='carousel-item grey-text' href='#three!'>
             <div className='icons'>
-              <i className='material-icons'>list</i>
-              <i className='material-icons'>chat</i>
-              <i className='material-icons'>call_split</i>
+              <i className='material-icons large'>list</i>
+              <i className='material-icons large'>dialpad</i>
+              <i className='material-icons large'>chat_bubble_outline</i>
+              <i className='material-icons large'>call_split</i>
             </div>
             <h2>Questionnaire steps</h2>
-            <p className=''>Questionnaires are organized in steps. Each step type offers different features like multiple choice, numeric input or just an explanation . The comparison steps allow A/B testing.</p>
+            <p>Questionnaires are organized in steps. Each step type offers different features like multiple choice, numeric input or just an explanation . The comparison steps allow A/B testing.</p>
           </div>
         </div>
       </div>
