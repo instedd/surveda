4981d993a61b8f7dfd016080e77e2c0fbf44c55a
Ary Borenszweig <aborenszweig@manas.com.ar>
2017-03-02 17:22:24 -0300

    Validate numeric answers in mobile web form. Fixes #711

--- web/static/mobile_survey/js/components/steps/NumericStep.jsx
+++ web/static/mobile_survey/js/components/steps/NumericStep.jsx
@@ -2,6 +2,21 @@ import React, { Component, PropTypes } from 'react'
 import Prompt from '../Prompt'
 
 class NumericStep extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: '', valid: false}
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const { step } = this.props
+    const { value } = event.target
+    const intValue = parseInt(value)
+    const valid = (step.min == null || intValue >= step.min) && (step.max == null || intValue <= step.max)
+    this.setState({value, valid})
+  }
+
   render() {
     const { step } = this.props
     return (
@@ -9,10 +24,10 @@ class NumericStep extends Component {
         <Prompt text={step.prompt} />
         <br />
         <div>
-          <input type='number' min={step.min} max={step.max} />
+          <input type='number' value={this.state.value} onChange={this.handleChange} min={step.min} max={step.max} />
         </div>
         <br />
-        <input type='submit' value='Next' />
+        <input type='submit' value='Next' disabled={!this.state.valid} />
       </div>
     )
   }
