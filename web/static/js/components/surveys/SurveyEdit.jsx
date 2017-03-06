02bcf64ae22bdfbe80274355935bf03b2fdb781a
Juan F. Blandini <jblandini@manas.com.ar>
2017-03-06 10:40:52 -0300

    Fixes #715 [Survey] Usability Feedback - No Next Button after cutoff rules

--- web/static/js/components/surveys/SurveyEdit.jsx
+++ web/static/js/components/surveys/SurveyEdit.jsx
@@ -7,8 +7,6 @@ import * as channelsActions from '../../actions/channels'
 import * as questionnairesActions from '../../actions/questionnaires'
 import * as respondentGroupsActions from '../../actions/respondentGroups'
 import SurveyForm from './SurveyForm'
-import { Tooltip } from '../ui'
-import { launchSurvey } from '../../api'
 import * as routes from '../../routes'
 
 class SurveyEdit extends Component {
@@ -44,12 +42,6 @@ class SurveyEdit extends Component {
     }
   }
 
-  launchSurvey() {
-    const { projectId, surveyId, router } = this.props
-    launchSurvey(projectId, surveyId)
-      .then(() => router.push(routes.survey(projectId, surveyId)))
-  }
-
   render() {
     const { survey, projectId, project, questionnaires, dispatch, channels, respondentGroups, invalidRespondents } = this.props
 
@@ -65,20 +57,8 @@ class SurveyEdit extends Component {
       questionnaire = questionnaires[questionnaireIds[0]]
     }
 
-    let launchComponent = null
-    if (survey.state == 'ready' && !readOnly) {
-      launchComponent = (
-        <Tooltip text='Launch survey'>
-          <a className='btn-floating btn-large waves-effect waves-light green right mtop' onClick={() => this.launchSurvey()}>
-            <i className='material-icons'>play_arrow</i>
-          </a>
-        </Tooltip>
-      )
-    }
-
     return (
       <div className='white'>
-        {launchComponent}
         <SurveyForm survey={survey} respondentGroups={respondentGroups} invalidRespondents={invalidRespondents} projectId={projectId} questionnaires={questionnaires} channels={channels} dispatch={dispatch} questionnaire={questionnaire} readOnly={readOnly} />
       </div>
     )
