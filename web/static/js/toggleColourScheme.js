d52ac34702dee06854140fd2326b848469254b17
lmatayoshi <lmatayoshi@manas.com.ar>
2017-03-17 02:05:31 -0300

    For #750. Apply style for header's background color

--- web/static/js/toggleColourScheme.js
+++ web/static/js/toggleColourScheme.js
@@ -1,7 +1,12 @@
 export const toggleColourScheme = (scheme) => {
-  if (scheme == 'better_data_for_health') {
-    document.documentElement.style.setProperty('--header-color', 'blue')
-  } else {
-    document.documentElement.style.setProperty('--header-color', '#424242')
+  switch (scheme) {
+    case 'better_data_for_health':
+      document.documentElement.style.setProperty('--header-color', '#6648A2')
+      break
+    case 'default':
+      document.documentElement.style.setProperty('--header-color', '#424242')
+      break
+    default:
+      throw new Error(`unknown scheme`)
   }
 }
