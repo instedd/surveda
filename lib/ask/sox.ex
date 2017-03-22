0422fc48ec9e3d6f0d8c35b94778af2053fe7167
Santiago Palladino <spalladino@gmail.com>
2017-03-22 17:55:51 -0300

    Compress wav audio files on upload #777
    Use sox to convert wav files to mp3 on upload only. If sox fails,
    the file is saved as wav. Add sox and libsox fmt mp3 to containers.

---
+++ lib/ask/sox.ex
@@ -0,0 +1,18 @@
+defmodule Ask.Sox do
+
+  def convert(from_type, from_filename, to_type) do
+    try do
+      case System.cmd(sox_executable, ["-V1", "-t", from_type, from_filename, "-e", "signed-integer", "-r", "44100", "-t", to_type, "-c1", "-"]) do
+        {output, 0} -> {:ok, output}
+        {_, code} -> {:error, code}
+      end
+    rescue
+      e -> {:error, inspect(e)}
+    end
+  end
+
+  defp sox_executable do
+    Application.get_env(:ask, :sox)[:bin] |> System.find_executable
+  end
+
+end
