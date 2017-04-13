d2be0c376b2ad0377743702178b5b600fa84593a
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2017-04-13 18:05:02 -0300

    For #721. Change font size for options depending on number of characters

--- web/static/mobile_survey/js/components/Prompt.jsx
+++ web/static/mobile_survey/js/components/Prompt.jsx
@@ -2,9 +2,31 @@
 import React, { Component, PropTypes } from 'react'
 
 class Prompt extends Component {
+  classNameForPrompt(prompt: String) {
+    const length = prompt.length
+    let cssClass
+    switch (true) {
+      case (length < 100):
+        cssClass = 'prompt-length-less-100'
+        break
+      case (length < 200):
+        cssClass = 'prompt-length-less-200'
+        break
+      case (length < 300):
+        cssClass = 'prompt-length-less-300'
+        break
+      case (length < 400):
+        cssClass = 'prompt-length-less-400'
+        break
+      default:
+        cssClass = 'prompt-length-large'
+    }
+    return cssClass
+  }
+
   render() {
     const { text } = this.props
-    return <div className='prompt'><h1>{text}</h1></div>
+    return <div className={'prompt'}><h1 className={this.classNameForPrompt(text)}>{text}</h1></div>
   }
 }
 
@@ -13,4 +35,3 @@ Prompt.propTypes = {
 }
 
 export default Prompt
-
