242089900902a1975ede6b9b588ad1327e3e9b39
Ary Borenszweig <aborenszweig@manas.com.ar>
2017-05-09 09:43:14 -0300

    Uncomment accidentally commented 'errors.js'

--- web/static/js/errors.js
+++ web/static/js/errors.js
@@ -1,32 +1,32 @@
-// import Raven from 'raven-js'
-// import { config } from './config'
-// import { Unauthorized } from './api'
-// import * as routes from './routes'
+import Raven from 'raven-js'
+import { config } from './config'
+import { Unauthorized } from './api'
+import * as routes from './routes'
 
-// Raven.config(config.sentryDsn, {release: config.version}).install()
-// Raven.setUserContext({email: config.user})
+Raven.config(config.sentryDsn, {release: config.version}).install()
+Raven.setUserContext({email: config.user})
 
-// window.addEventListener('unhandledrejection', (e) => {
-//   if (e.reason instanceof Unauthorized) {
-//     window.location = routes.root
-//     // Ignore unauthorized errors
-//     return
-//   }
+window.addEventListener('unhandledrejection', (e) => {
+  if (e.reason instanceof Unauthorized) {
+    window.location = routes.root
+    // Ignore unauthorized errors
+    return
+  }
 
-//   onError(e)
-//   if (e.reason instanceof Error) {
-//     Raven.captureException(e.reason)
-//   } else {
-//     Raven.captureMessage('Unhandled promise rejection', {extra: {reason: e.reason}})
-//   }
-// })
+  onError(e)
+  if (e.reason instanceof Error) {
+    Raven.captureException(e.reason)
+  } else {
+    Raven.captureMessage('Unhandled promise rejection', {extra: {reason: e.reason}})
+  }
+})
 
-// window.addEventListener('error', (e) => {
-//   onError(e)
-// })
+window.addEventListener('error', (e) => {
+  onError(e)
+})
 
-// const onError = (e) => {
-//   event.preventDefault()
-//   console.log(event)
-//   $('#unhandledError').modal('open')
-// }
+const onError = (e) => {
+  event.preventDefault()
+  console.log(event)
+  $('#unhandledError').modal('open')
+}
