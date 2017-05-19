09ee0749ce55ff8c152efa8785d7a7008f26f5d8
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-05-19 16:27:35 -0300

    New style for language list (closes #1007)

--- web/static/js/components/questionnaires/LanguagesList.jsx
+++ web/static/js/components/questionnaires/LanguagesList.jsx
@@ -40,72 +40,67 @@ class LanguagesList extends Component {
     dispatch(actions.setActiveLanguage(language))
   }
 
-  render() {
-    const { loading, languages, defaultLanguage, activeLanguage, onRemoveLanguage, errors, readOnly } = this.props
+  renderLanguageRow(code, name, isDefault = false) {
+    const { activeLanguage, readOnly, onRemoveLanguage, errors } = this.props
+    const isActiveLanguage = code == activeLanguage
+    const buttons = []
 
+    if (isActiveLanguage) {
+      buttons.push(<span key='active-mark' className='active-mark'>
+        <i className='material-icons'>done</i>
+      </span>)
+    }
+
+    if (!readOnly && !isActiveLanguage && !isDefault) {
+      buttons.push(<span key='remove-language' className='remove-language' onClick={() => onRemoveLanguage(code)}>
+        <i className='material-icons'>highlight_off</i>
+      </span>)
+    }
+
+    if (!readOnly && !isDefault) {
+      buttons.push(<span key='set-default-language' className='set-default-language' onClick={this.defaultLanguageSelected(code)}>
+        <i className='material-icons'>arrow_upward</i>
+      </span>)
+    }
+
+    return <li key={code} onClick={e => this.setActiveLanguage(e, code)}
+      className={classNames({'active-language': code == activeLanguage, 'tooltip-error': hasErrorsInLanguage(errors, code)})}>
+      {buttons}
+      <span className='language-name' title={name}>
+        {name}
+      </span>
+    </li>
+  }
+
+  render() {
+    const { loading, languages, defaultLanguage, readOnly } = this.props
     if (loading) {
       return <div>Loading...</div>
     }
 
-    let otherLanguages = languages.filter((lang) => lang !== defaultLanguage)
-    otherLanguages = otherLanguages.map((lang: string) => [lang, this.translateLangCode(lang)])
-    otherLanguages = otherLanguages.sort((l1, l2) => (l1[1] <= l2[1]) ? -1 : 1)
-    otherLanguages = otherLanguages.map((lang) =>
-      <li key={lang[0]} className={classNames({'active-language': lang[0] == activeLanguage, 'tooltip-error': hasErrorsInLanguage(errors, lang[0])})}>
-        {readOnly
-        ? null
-        : <div>
-          <span className='remove-language' onClick={() => onRemoveLanguage(lang[0])}>
-            <i className='material-icons'>highlight_off</i>
-          </span>
-          <span className='set-default-language' onClick={this.defaultLanguageSelected(lang[0])}>
-            <i className='material-icons'>arrow_upward</i>
-          </span>
+    const LanguageSection = ({title, children}) =>
+      <div className='row'>
+        <div className='col s12'>
+          <p className='grey-text'>{title}:</p>
+          <ul className='languages-list'>{children}</ul>
         </div>
-        }
-        <span className='language-name' title={this.translateLangCode(lang[0])}>
-          {this.translateLangCode(lang[0])}
-        </span>
-        <span href='#' className='right-arrow' onClick={e => this.setActiveLanguage(e, lang[0])}>
-          <i className='material-icons'>keyboard_arrow_right</i>
-        </span>
-      </li>
-      )
+      </div>
+
+    let otherLanguages = languages.filter((lang) => lang !== defaultLanguage)
+      .map((lang: string) => [lang, this.translateLangCode(lang)])
+      .sort(([c1, n1], [c2, n2]) => n1.localeCompare(n2))
+      .map(([code, name]) => this.renderLanguageRow(code, name))
 
     let otherLanguagesComponent = null
     if (otherLanguages.length != 0) {
-      otherLanguagesComponent = (
-        <div className='row'>
-          <div className='col s12'>
-            <p className='grey-text'>Other languages:</p>
-            <ul className='other-languages-list'>
-              {otherLanguages}
-            </ul>
-          </div>
-        </div>
-        )
+      otherLanguagesComponent = <LanguageSection title='Other languages'>{otherLanguages}</LanguageSection>
     }
-
-    const defaultLanguageClassnames = classNames({
-      'selected-language': true,
-      'active-language': activeLanguage == defaultLanguage
-    })
 
     return (
       <div className='languages'>
-        <div className='row'>
-          <div className='col s12'>
-            <p className='grey-text'>Primary language:</p>
-            <ul className={defaultLanguageClassnames}>
-              <li className={classNames({'tooltip-error': hasErrorsInLanguage(errors, defaultLanguage)})}>
-                <span>{this.translateLangCode(defaultLanguage)}</span>
-                <span href='#' className='right-arrow' onClick={e => this.setActiveLanguage(e, defaultLanguage)}>
-                  <i className='material-icons'>keyboard_arrow_right</i>
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <LanguageSection title='Primary language:'>
+          {this.renderLanguageRow(defaultLanguage, this.translateLangCode(defaultLanguage), true)}
+        </LanguageSection>
         {otherLanguagesComponent}
         {readOnly
         ? null
