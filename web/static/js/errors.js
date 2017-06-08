4010cbf9f34363381ba00736aeec91ce1edfeeb8
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-06-08 16:52:03 -0300

    Error dialog with details (closes #1019)

--- web/static/js/errors.js
+++ web/static/js/errors.js
@@ -2,31 +2,60 @@ import Raven from 'raven-js'
 import { config } from './config'
 import { Unauthorized } from './api'
 import * as routes from './routes'
+import React from 'react'
+import { render } from 'react-dom'
+import { showError } from 'components/ui/ErrorModal'
 
 Raven.config(config.sentryDsn, {release: config.version}).install()
 Raven.setUserContext({email: config.user})
 
 window.addEventListener('unhandledrejection', (e) => {
+  e.preventDefault()
+  console.log(e)
+
   if (e.reason instanceof Unauthorized) {
     window.location = routes.root
     // Ignore unauthorized errors
     return
   }
 
-  onError(e)
   if (e.reason instanceof Error) {
     Raven.captureException(e.reason)
+    onError(e.reason)
   } else {
     Raven.captureMessage('Unhandled promise rejection', {extra: {reason: e.reason}})
+    onError(new Error('Unhandled promise rejection'))
   }
 })
 
 window.addEventListener('error', (e) => {
-  onError(e)
+  e.preventDefault()
+  console.log(e)
+  onError(e.error)
 })
 
-const onError = (e) => {
-  event.preventDefault()
-  console.log(event)
-  $('#unhandledError').modal('open')
+const onError = (error) => {
+  const errorId = Raven.lastEventId()
+
+  const onDetails = (e) => {
+    e.preventDefault()
+    $('.error-toast').remove()
+    showError(errorId, error)
+  }
+
+  const onReload = (e) => {
+    e.preventDefault()
+    window.location.href = '/'
+  }
+
+  const message = <div style={{width: '500px'}}>
+    Something went wrong
+    <a className='btn-flat red-text' onClick={onDetails}>DETAILS</a>
+    <a className='btn-flat red-text' onClick={onReload}>RELOAD</a>
+  </div>
+
+  const messageDom = document.createElement('div')
+  render(message, messageDom)
+  window.Materialize.toast(messageDom, null, 'error-toast')
 }
+
