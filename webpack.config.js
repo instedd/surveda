bcb33bf8e1d61d9489bfd01fe9768a0f952d10c7
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-06-07 23:59:55 -0300

    Refactored webpack config into a single module with separate chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,9 @@
 var webpack = require('webpack')
+var path = require('path')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
-var path = require('path')
 
-var moduleConfig = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: [
-        /node_modules/,
-        /static\/vendor/
-      ],
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015', 'react', 'stage-0', 'flow'],
-          plugins: ['transform-object-rest-spread']
-        }
-      }
-    },
-    {
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract({use: 'css-loader'})
-    },
-    {
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              url: false
-            }
-          },
-          {
-            loader: 'sass-loader?sourceMap',
-            options: {
-              includePaths: [
-                path.join(__dirname, '/web/static/vendor/css')
-              ]
-            }
-          }
-        ]
-      })
-    }
-  ]
-}
-
-var app = {
+module.exports = {
   resolve: {
     modules: [
       path.join(__dirname, '/node_modules'),
@@ -56,23 +12,71 @@ var app = {
     extensions: ['.js', '.jsx']
   },
 
-  entry: [
-    './web/static/vendor/js/materialize.js',
-    './web/static/js/app.jsx',
-    './web/static/css/app.scss'
-  ],
+  entry: {
+    app: [
+      './web/static/vendor/js/materialize.js',
+      './web/static/js/app.jsx',
+      './web/static/css/app.scss'
+    ],
+    mobileSurvey: [
+      './web/static/mobile_survey/js/mobileSurvey.jsx',
+      './web/static/mobile_survey/css/mobile.scss'
+    ]
+  },
 
   output: {
     path: path.join(__dirname, '/priv/static'),
-    filename: 'js/app.js'
+    filename: 'js/[name].js'
   },
 
-  module: moduleConfig,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [
+          /node_modules/,
+          /static\/vendor/
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react', 'stage-0', 'flow'],
+            plugins: ['transform-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({use: 'css-loader'})
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                url: false
+              }
+            },
+            {
+              loader: 'sass-loader?sourceMap',
+              options: {
+                includePaths: [
+                  path.join(__dirname, '/web/static/vendor/css')
+                ]
+              }
+            }
+          ]
+        })
+      }
+    ]
+  },
 
   devtool: 'cheap-module-source-map',
 
   plugins: [
-    new ExtractTextPlugin('css/app.css'),
+    new ExtractTextPlugin('css/[name].css'),
     new CopyWebpackPlugin([{ from: './web/static/assets' }]),
     new webpack.ProvidePlugin({
       'window.jQuery': 'jquery',
@@ -82,33 +86,3 @@ var app = {
     })
   ]
 }
-
-var mobileSurvey = {
-  resolve: {
-    modules: [
-      path.join(__dirname, '/node_modules'),
-      path.join(__dirname, '/web/static/js')
-    ],
-    extensions: ['.js', '.jsx']
-  },
-
-  entry: [
-    './web/static/mobile_survey/js/mobileSurvey.jsx',
-    './web/static/mobile_survey/css/mobile.scss'
-  ],
-
-  output: {
-    path: path.join(__dirname, '/priv/static'),
-    filename: 'js/mobileSurvey.js'
-  },
-
-  module: moduleConfig,
-
-  devtool: 'cheap-module-source-map',
-
-  plugins: [
-    new ExtractTextPlugin('css/mobileSurvey.css')
-  ]
-}
-
-module.exports = [app, mobileSurvey]
