dbc2ba9b457a3473728b76c82ec9f3e7a306d0ef
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-06-09 12:22:00 -0300

    Tell webpack to expose jQuery as global $ variable (fixes #1067)

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,13 @@ module.exports = {
             }
           ]
         })
+      },
+      {
+        test: require.resolve('jquery'),
+        use: [{
+          loader: 'expose-loader',
+          options: '$'
+        }]
       }
     ]
   },
