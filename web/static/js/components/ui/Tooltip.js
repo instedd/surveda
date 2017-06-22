0fcfea27844032bfe4c7ae720dc1b2e9ad8830f8
Mariano Coca <marianoabelcoca@gmail.com>
2017-06-22 10:56:03 -0300

    Second attempt of tooltip fixing, with a slight throttling.

--- web/static/js/components/ui/Tooltip.js
+++ web/static/js/components/ui/Tooltip.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import throttle from 'lodash/throttle'
 
 export class Tooltip extends Component {
-  static defaultProps = {
-    position: 'top',
-    delay: 20
-  }
-
+  recalculate: Function
   static propTypes = {
     children: React.PropTypes.node.isRequired,
     position: React.PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
@@ -15,21 +12,43 @@ export class Tooltip extends Component {
     className: React.PropTypes.string
   }
 
+  static defaultProps = {
+    position: 'top',
+    delay: 20
+  }
+
+  constructor(props: Props) {
+    super(props)
+    this.recalculate = throttle(this.recalculate.bind(this), 15)
+  }
+
   componentDidMount() {
     this.updateTooltip()
+    window.addEventListener('scroll', this.recalculate)
+    this.recalculate()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.recalculate)
+    const node = ReactDOM.findDOMNode(this.refs.node)
+    $(node).tooltip('remove')
   }
 
   componentDidUpdate() {
     this.updateTooltip()
+    this.recalculate()
   }
 
   updateTooltip() {
-    const { position } = this.props
     const node = ReactDOM.findDOMNode(this.refs.node)
-    const origin = $(node)
-    const tooltip = origin.tooltip()[0]
+    const tooltip = $(node).tooltip()[0]
     const tooltipId = tooltip.getAttribute('data-tooltip-id')
     $(`#${tooltipId}`).addClass(this.props.className)
+  }
+
+  recalculate() {
+    const { position } = this.props
+    const origin = $(ReactDOM.findDOMNode(this.refs.node))
     const tootlipElement = $('.material-tooltip')
     const margin = 5
     var tooltipHeight = tootlipElement.outerHeight()
@@ -38,29 +57,27 @@ export class Tooltip extends Component {
     var originHeight = origin.outerHeight()
     var targetTop, targetLeft, newCoordinates
 
-    $(document).on('scroll', (context, args) => {
-      if (position === 'top') {
-        targetTop = origin.offset().top - tooltipHeight - margin
-        targetLeft = origin.offset().left + originWidth / 2 - tooltipWidth / 2
-        newCoordinates = this.repositionWithinScreen(targetLeft, targetTop, tooltipWidth, tooltipHeight)
-      } else if (position === 'left') {
-        targetTop = origin.offset().top + originHeight / 2 - tooltipHeight / 2
-        targetLeft = origin.offset().left - tooltipWidth - margin
-        newCoordinates = this.repositionWithinScreen(targetLeft, targetTop, tooltipWidth, tooltipHeight)
-      } else if (position === 'right') {
-        targetTop = origin.offset().top + originHeight / 2 - tooltipHeight / 2
-        targetLeft = origin.offset().left + originWidth + margin
-        newCoordinates = this.repositionWithinScreen(targetLeft, targetTop, tooltipWidth, tooltipHeight)
-      } else {
-        // Bottom Position
-        targetTop = origin.offset().top + origin.outerHeight() + margin
-        targetLeft = origin.offset().left + originWidth / 2 - tooltipWidth / 2
-        newCoordinates = this.repositionWithinScreen(targetLeft, targetTop, tooltipWidth, tooltipHeight)
-      }
-      tootlipElement.css({
-        top: newCoordinates.y,
-        left: newCoordinates.x
-      })
+    if (position === 'top') {
+      targetTop = origin.offset().top - tooltipHeight - margin
+      targetLeft = origin.offset().left + originWidth / 2 - tooltipWidth / 2
+      newCoordinates = this.repositionWithinScreen(targetLeft, targetTop, tooltipWidth, tooltipHeight)
+    } else if (position === 'left') {
+      targetTop = origin.offset().top + originHeight / 2 - tooltipHeight / 2
+      targetLeft = origin.offset().left - tooltipWidth - margin
+      newCoordinates = this.repositionWithinScreen(targetLeft, targetTop, tooltipWidth, tooltipHeight)
+    } else if (position === 'right') {
+      targetTop = origin.offset().top + originHeight / 2 - tooltipHeight / 2
+      targetLeft = origin.offset().left + originWidth + margin
+      newCoordinates = this.repositionWithinScreen(targetLeft, targetTop, tooltipWidth, tooltipHeight)
+    } else {
+      // Bottom Position
+      targetTop = origin.offset().top + origin.outerHeight() + margin
+      targetLeft = origin.offset().left + originWidth / 2 - tooltipWidth / 2
+      newCoordinates = this.repositionWithinScreen(targetLeft, targetTop, tooltipWidth, tooltipHeight)
+    }
+    tootlipElement.css({
+      top: newCoordinates.y,
+      left: newCoordinates.x
     })
   }
 
@@ -78,11 +95,6 @@ export class Tooltip extends Component {
     }
 
     return {x: x, y: y}
-  }
-
-  componentWillUnmount() {
-    const node = ReactDOM.findDOMNode(this.refs.node)
-    $(node).tooltip('remove')
   }
 
   render() {
