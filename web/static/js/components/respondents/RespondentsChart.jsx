725286b794004a5598ae475d463844db7d603a78
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-08-03 19:15:06 -0300

    Fixed warnings generated from survey charts. Removed react-d3-library as it is not needed to integrate d3 into React. Remove unused d3-time library.

--- web/static/js/components/respondents/RespondentsChart.jsx
+++ web/static/js/components/respondents/RespondentsChart.jsx
@@ -1,169 +1,133 @@
-import React, { Component } from 'react'
-import rd3 from 'react-d3-library'
+import React, { PureComponent } from 'react'
 import { referenceStrokeColorClasses } from '../../referenceColors'
-const RD3Component = rd3.Component
+import d3 from 'd3'
 
-class RespondentsChart extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { d3: '' }
-  }
-
+class RespondentsChart extends PureComponent {
   static propTypes = {
     cumulativePercentages: React.PropTypes.object.isRequired
   }
 
   componentDidMount() {
-    this.width = this.refs.container.offsetWidth
-    this.height = this.width * (1 / 2)
-    this.chartMargin = {top: 0, bottom: 20, left: 0, right: 0}
+    this.width = this.refs.main.offsetWidth
+    this.height = this.width / 2
+    this.chartMargin = { top: 0, bottom: 20, left: 0, right: 0 }
     this.chartWidth = this.width - this.chartMargin.left - this.chartMargin.right
     this.chartHeight = this.height - this.chartMargin.top - this.chartMargin.bottom
     this.renderD3()
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setData(nextProps.cumulativePercentages)
+  componentDidUpdate() {
+    this.renderD3()
   }
 
-  setData(cumulativePercentages) {
-    let initialDate
-    let nextThreeMonths
-    let lastDate
-    let totalQuestionnaires = Object.keys(cumulativePercentages).length
+  buildPaths(container, total) {
+    let paths = []
+    for (let i = 0; i < total; ++i) {
+      paths.push(container.append('path').attr('class', 'line'))
+    }
+
+    return paths
+  }
+
+  renderD3() {
+    const { cumulativePercentages } = this.props
+    const svg = d3.select(this.refs.svg)
+    const container = d3.select(this.refs.container)
+    const backgroundPath = d3.select(this.refs.backgroundPath)
+    const yaxisContainer = d3.select(this.refs.yaxis)
+    const xaxisContainer = d3.select(this.refs.xaxis)
+      .attr('transform', 'translate(0,' + (this.chartHeight) + ')')
+
+    const xScale = d3.time.scale()
+    const yScale = d3.scale.linear().domain([0, 100])
+
+    // Set size
+    xScale.range([0, this.chartWidth])
+    yScale.range([this.chartHeight, 0])
+
+    // Set data
+    let initialDate, lastDate
+    const totalQuestionnaires = Object.keys(cumulativePercentages).length
+
     if (!cumulativePercentages || totalQuestionnaires < 1) {
-      initialDate = new Date(Date.now())
-      lastDate = new Date(Date.now())
+      initialDate = new Date()
+      lastDate = new Date()
       lastDate.setDate(lastDate.getDate() + 90)
     } else {
       // Uses random one because all questionnaires have the same range of dates.
-      let randomQuestionnaireId = Object.keys(cumulativePercentages)[0]
-      let randomQuestionnaireByDate = cumulativePercentages[randomQuestionnaireId]
+      const randomQuestionnaireId = Object.keys(cumulativePercentages)[0]
+      const randomQuestionnaireByDate = cumulativePercentages[randomQuestionnaireId]
       initialDate = new Date(Date.parse(randomQuestionnaireByDate[0].date))
-      nextThreeMonths = new Date(Date.parse(randomQuestionnaireByDate[0].date))
+      const nextThreeMonths = new Date(Date.parse(randomQuestionnaireByDate[0].date))
       nextThreeMonths.setDate(nextThreeMonths.getDate() + 90)
       lastDate = new Date(Math.max(Date.parse(randomQuestionnaireByDate[randomQuestionnaireByDate.length - 1].date), nextThreeMonths))
     }
-    const formatDate = function(date) { return new Date(Date.parse(date)) }
-    this.datas = Object.entries(cumulativePercentages).map((entry) => {
+
+    const formatDate = date => new Date(Date.parse(date))
+    const datas = Object.entries(cumulativePercentages).map((entry) => {
       let completedPercentagesByDate = entry[1]
-      return completedPercentagesByDate.map((v) => {
-        return { date: formatDate(v.date), percent: Number(v.percent) }
-      })
-    });
+      return completedPercentagesByDate.map(v => (
+        { date: formatDate(v.date), percent: Number(v.percent) }
+      ))
+    })
 
-    (this._x).domain([initialDate, lastDate])
+    xScale.domain([initialDate, lastDate])
 
-    this.xaxis = d3.svg.axis()
-                        .scale(this._x)
-                        .ticks(3)
+    const xaxis = d3.svg.axis()
+      .scale(xScale)
+      .ticks(3)
 
-    const _x = this._x
-    const _y = this._y
-    this.line = d3.svg.line()
-                        .x(function(d) { return _x(d.date) })
-                        .y(function(d) { return _y(d.percent) })
+    const yaxis = d3.svg.axis()
+      .scale(yScale)
+      .tickSize(this.width)
+      .ticks(4)
+      .orient('right')
 
-    this.backgroundData = [{date: initialDate, percent: 0}, {date: lastDate, percent: 100}]
-
-    this.XAxis.call(this.xaxis
+    xaxisContainer.call(xaxis
         .ticks(3)
         .tickFormat(d3.time.format('%b')))
         .selectAll('text')
         .attr('dy', 7)
         .attr('x', 10)
 
-    this.paths = this.buildPaths(totalQuestionnaires)
-    let referenceClasses = referenceStrokeColorClasses(totalQuestionnaires)
-    this.datas.forEach((data, index) => {
-      this.paths[index].datum(data)
-          .attr('class', `line respondentsData ${referenceClasses[index]}`)
-          .attr('d', this.line)
-    })
-
-    this.backgroundPath.datum(this.backgroundData)
-        .attr('class', 'line backgroundData')
-        .style('stroke-dasharray', '2,2')
-        .attr('d', this.line)
-  }
-
-  buildPaths(total) {
-    let paths = []
-    for (let i = 0; i < total; ++i) {
-      paths.push(this.container.append('path')
-                               .attr('class', 'line'))
-    }
-
-    return paths
-  }
-
-  setSize() {
-    this._x.range([0, this.chartWidth])
-    this._y.range([this.chartHeight, 0])
-  }
-
-  init(totalQuestionnaires) {
-    this.container = this.svg.append('g')
-    this.YAxis = this.container.append('g')
-                          .attr('class', 'y axis')
-    this.XAxis = this.container.append('g')
-                          .attr('class', 'x axis')
-                          .attr('transform', 'translate(0,' + (this.chartHeight) + ')')
-
-    this.backgroundPath = this.container.append('path')
-                                    .attr('class', 'line')
-
-    this._x = d3.time.scale()
-    this._y = d3.scale.linear().domain([0, 100])
-
-    this.yaxis = d3.svg.axis()
-                        .scale(this._y)
-                        .tickSize(this.width)
-                        .ticks(4)
-                        .orient('right')
-    this.xaxis = d3.svg.axis()
-                        .scale(this._x)
-
-    const _x = this._x
-    const _y = this._y
-    this.line = d3.svg.line()
-                        .x(function(d) { return _x(d.date) })
-                        .y(function(d) { return _y(d.percent) })
-  }
-
-  renderD3() {
-    if (!this.width) {
-      return
-    }
-
-    const { cumulativePercentages } = this.props
-    const node = document.createElement('div')
-    this.svg = d3.select(node).append('svg')
-
-    let totalQuestionnaires = Object.keys(cumulativePercentages).length
-    this.init(totalQuestionnaires)
-    this.setSize()
-    this.setData(cumulativePercentages)
-
-    this.svg.attr('width', this.width)
-        .attr('height', this.height)
-
-    this.YAxis.call(this.yaxis)
+    yaxisContainer.call(yaxis)
         .selectAll('text')
           .attr('x', 0)
           .attr('dy', 16)
 
-    this.XAxis.call(this.xaxis)
-        .selectAll('text')
-        .attr('dy', 7)
+    const line = d3.svg.line()
+      .x(d => xScale(d.date))
+      .y(d => yScale(d.percent))
 
-    this.setState({node: node})
+    const paths = this.buildPaths(container, totalQuestionnaires)
+    let referenceClasses = referenceStrokeColorClasses(totalQuestionnaires)
+    datas.forEach((data, index) => {
+      paths[index].datum(data)
+          .attr('class', `line respondentsData ${referenceClasses[index]}`)
+          .attr('d', line)
+    })
+
+    const backgroundData = [{date: initialDate, percent: 0}, {date: lastDate, percent: 100}]
+    backgroundPath.datum(backgroundData)
+      .attr('class', 'line backgroundData')
+      .style('stroke-dasharray', '2,2')
+      .attr('d', line)
+
+    svg
+      .attr('width', this.width)
+      .attr('height', this.height)
   }
 
   render() {
     return (
-      <div ref='container'>
-        <RD3Component data={this.state.node} />
+      <div ref='main'>
+        <svg ref='svg'>
+          <g ref='container'>
+            <g className='y axis' ref='yaxis' />
+            <g className='x axis' ref='xaxis' />
+            <path className='line' ref='backgroundPath' />
+          </g>
+        </svg>
       </div>
     )
   }
