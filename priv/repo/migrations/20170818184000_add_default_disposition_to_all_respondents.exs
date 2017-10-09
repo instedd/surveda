889e4368cf954054b0a0924523f21917f6d0e209
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-10-09 18:37:30 -0300

    Fixed migration

--- priv/repo/migrations/20170818184000_add_default_disposition_to_all_respondents.exs
+++ priv/repo/migrations/20170818184000_add_default_disposition_to_all_respondents.exs
@@ -2,26 +2,31 @@ defmodule Ask.Repo.Migrations.AddDefaultDispositionToAllRespondents do
   use Ecto.Migration
   alias Ask.Repo
 
-  def change do
+  def up do
     Repo.transaction(fn ->
       Repo.query!("""
         UPDATE respondents
         SET disposition = 'contacted'
         WHERE state = 'cancelled'
+        AND disposition IS NULL
         """, [])
 
       Repo.query!("""
         UPDATE respondents
         SET disposition = 'failed'
         WHERE state = 'failed'
+        AND disposition IS NULL
         """, [])
 
       Repo.query!("""
         UPDATE respondents
         SET disposition = 'registered'
         WHERE state = 'pending'
+        AND disposition IS NULL
         """, [])
     end)
   end
 
+  def down do
+  end
 end
