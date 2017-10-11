df422ef473ead2a27ff4260fa420f713eced27e0
Mariano Coca <marianoabelcoca@gmail.com>
2017-10-11 17:09:56 -0300

    Fix old migration

--- priv/repo/migrations/20161013154229_set_default_schedule_day_of_week_for_surveys.exs
+++ priv/repo/migrations/20161013154229_set_default_schedule_day_of_week_for_surveys.exs
@@ -2,6 +2,19 @@ defmodule Ask.Repo.Migrations.SetDefaultScheduleDayOfWeekForSurveys do
   use Ecto.Migration
   import Ecto.Query
 
+  defmodule Survey do
+    use Ask.Web, :model
+
+    schema "surveys" do
+      field :schedule_day_of_week, :integer
+    end
+
+    def changeset(struct, params \\ %{}) do
+      struct
+      |> cast(params, [:schedule_day_of_week])
+    end
+  end
+
   def change do
     from(s in Ask.Survey, where: is_nil(s.schedule_day_of_week))
     |> Ask.Repo.update_all(set: [schedule_day_of_week: 127]) # 127 was every_day in the old ecto type
