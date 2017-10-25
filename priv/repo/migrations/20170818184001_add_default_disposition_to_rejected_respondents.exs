c656d25b629dd47a577831ed456aeed5733c2230
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-10-25 16:29:07 -0300

    Set missing disposition for all respondents in 'rejected' state

---
+++ priv/repo/migrations/20170818184001_add_default_disposition_to_rejected_respondents.exs
@@ -0,0 +1,18 @@
+defmodule Ask.Repo.Migrations.AddDefaultDispositionToRejectedRespondents do
+  use Ecto.Migration
+  alias Ask.Repo
+
+  def up do
+    Repo.transaction(fn ->
+      Repo.query!("""
+        UPDATE respondents
+        SET disposition = 'rejected'
+        WHERE state = 'rejected'
+        AND disposition IS NULL
+        """, [])
+    end)
+  end
+
+  def down do
+  end
+end
