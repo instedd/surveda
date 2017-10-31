c812c676045debac25da816ba40f29ed5331b0a9
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-10-31 17:29:29 -0300

    Improve performance of results downloading for huge surveys (fixes #1199)

--- web/helpers/csv_helper.ex
+++ web/helpers/csv_helper.ex
@@ -2,24 +2,18 @@ defmodule CSV.Helper do
   import Plug.Conn
   @chunk_lines 100
 
-  alias Ask.Repo
-
   def csv_stream(conn, rows, filename) do
     conn = conn
       |> put_resp_content_type("text/csv")
       |> put_resp_header("content-disposition", "attachment; filename=\"#{filename}\"")
       |> send_chunked(200)
 
-    {:ok, conn} = Repo.transaction(fn() ->
-      rows
-      |> CSV.encode
-      |> Stream.chunk(@chunk_lines, @chunk_lines, [])
-      |> Enum.reduce(conn, fn (lines, conn) ->
-        {:ok, conn} = chunk(conn, lines)
-        conn
-      end)
+    rows
+    |> CSV.encode
+    |> Stream.chunk(@chunk_lines, @chunk_lines, [])
+    |> Enum.reduce(conn, fn (lines, conn) ->
+      {:ok, conn} = chunk(conn, lines)
+      conn
     end)
-
-    conn
   end
 end
