ba243a26f51f1c91d88883e310489d5d7d96451c
Mariano Coca <marianoabelcoca@gmail.com>
2017-11-17 20:05:32 -0300

    Add highlight to pre-commit script to facilitate output reading

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
-echo "Running Mix tests"
+echo "----==== Running Mix tests ====----"
 MIX_TESTS="$(docker-compose run --rm app mix test)"
 
+NC='\033[0m'
+GREEN='\033[0;32m'
+
 if [ $? -eq 0 ]; then
-   echo "OK";
+  echo "${GREEN}OK${NC}";
 else
   echo "${MIX_TESTS}"
 fi
 
-echo "Running JS tests"
+echo "----==== Running JS tests ====----"
 JS_TESTS="$(docker-compose run --rm webpack yarn test)"
 
 if [ $? -eq 0 ]; then
-  echo "OK";
+  echo "${GREEN}OK${NC}";
 else
   echo "${JS_TESTS}"
 fi
 
-echo "Running Flow tests"
+echo "----==== Running Flow tests ====----"
 if hash flow 2>/dev/null; then
   FLOW_TESTS="$(flow check)"
 else
@@ -25,16 +28,16 @@ else
 fi
 
 if [ $? -eq 0 ]; then
-  echo "OK";
+  echo "${GREEN}OK${NC}";
 else
   echo "${FLOW_TESTS}"
 fi
 
-echo "Running Eslint tests"
+echo "----==== Running Eslint tests ====----"
 ESLINT_TESTS="$(docker-compose run --rm webpack yarn eslint)"
 
 if [ $? -eq 0 ]; then
-  echo "OK";
+  echo "${GREEN}OK${NC}";
 else
   echo "${ESLINT_TESTS}"
 fi
