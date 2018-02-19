instedd/surveda

commit 9dc030c64c6cb34b3514cd0a39f36fac5a888a94

Small refactor to the LanguageSelector component, to reuse some code Related with #1217

web/static/js/components/layout/LanguageSelector.jsx
@@ -19,11 +19,11 @@ class LanguageSelector extends Component {
   languageNameFromCode(code) {
     switch (code) {
       case 'en':
-        return 'English (en)'
+        return 'English'
       case 'es':
-        return 'Español (es)'
+        return 'Español'
       case 'fr':
-        return 'Français (fr)'
+        return 'Français'
       default:
         return ''
     }
@@ -34,32 +34,31 @@ class LanguageSelector extends Component {
     const currentLanguage = i18n.language
 
     return (
-          <Dropdown className={this.props.className} label={this.languageNameFromCode(currentLanguage)} icon='language' iconLeft >
-            <DropdownDivider />
-            <DropdownItem>
-              <a onClick={() => this.changeLanguage('en')}>
-                <i className='material-icons' style={{opacity: `${currentLanguage == 'en' ? 1 : 0}`}} >done</i>
-                English
-                <span className='lang'>en</span>
-              </a>
-            </DropdownItem>
-            <DropdownItem>
-              <a onClick={() => this.changeLanguage('es')}>
-                <i className='material-icons' style={{opacity: `${currentLanguage == 'es' ? 1 : 0}`}} >done</i>
-                Español
-                <span className='lang'>es</span>
-              </a>
-            </DropdownItem>
-            <DropdownItem>
-              <a onClick={() => this.changeLanguage('fr')}>
-                <i className='material-icons' style={{opacity: `${currentLanguage == 'fr' ? 1 : 0}`}} >done</i>
-                Français
-                <span className='lang'>fr</span>
-              </a>
-            </DropdownItem>
-          </Dropdown>
+      <Dropdown className={'languageSelector'} label={`${this.languageNameFromCode(currentLanguage)} (${currentLanguage})`} icon='language' iconLeft >
+        <DropdownDivider />
+        <LanguageItem languageCode='en' currentLanguage={currentLanguage} langSelector={this} />
+        <LanguageItem languageCode='es' currentLanguage={currentLanguage} langSelector={this} />
+        <LanguageItem languageCode='fr' currentLanguage={currentLanguage} langSelector={this} />
+      </Dropdown>
     )
   }
+}
+
+const LanguageItem = ({languageCode, currentLanguage, langSelector}) => (
+  <DropdownItem>
+    <a onClick={() => langSelector.changeLanguage(languageCode)}>
+      <i className='material-icons' style={{opacity: `${currentLanguage == languageCode ? 1 : 0}`}} >done</i>
+      {langSelector.languageNameFromCode(currentLanguage)}
+      <span className='lang'>{languageCode}</span>
+    </a>
+  </DropdownItem>
+)
+
+LanguageItem.propTypes = {
+  langSelector: PropTypes.object,
+  languageCode: PropTypes.string,
+  languageName: PropTypes.string,
+  currentLanguage: PropTypes.string
 }
 
 LanguageSelector.propTypes = {

web/static/js/components/ui/Dropdown.jsx
@@ -22,7 +22,7 @@ export class Dropdown extends Component {
   }
 
   render() {
-    const { label, icon = 'arrow_drop_down', dataBelowOrigin = true, children, constrainWidth = true, className, readOnly,iconLeft } = this.props
+    const { label, icon = 'arrow_drop_down', dataBelowOrigin = true, children, constrainWidth = true, className, readOnly, iconLeft = false } = this.props
     const onButtonClick = (event) => {
       event.preventDefault()
     }

commit bb313df518855eddc13a90a7323c79ee684f56aa

Small fix to the label shown in the LanguageSelector.jsx Error introduced in 9dc030c64c6cb34b3514cd0a39f36fac5a888a94 Related with #1217

web/static/js/components/layout/LanguageSelector.jsx
@@ -48,7 +48,7 @@ const LanguageItem = ({languageCode, currentLanguage, langSelector}) => (
   <DropdownItem>
     <a onClick={() => langSelector.changeLanguage(languageCode)}>
       <i className='material-icons' style={{opacity: `${currentLanguage == languageCode ? 1 : 0}`}} >done</i>
-      {langSelector.languageNameFromCode(currentLanguage)}
+      {langSelector.languageNameFromCode(languageCode)}
       <span className='lang'>{languageCode}</span>
     </a>
   </DropdownItem>