f4537486cda545b74b9912c035c2833bc1aae99b
Juan Wajnerman <jwajnerman@manas.com.ar>
2018-10-06 15:25:15 -0300

    Version bump (0.19.0)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ask.Mixfile do
 
   def project do
     [app: :ask,
-     version: "0.17.0",
+     version: "0.19.0",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
