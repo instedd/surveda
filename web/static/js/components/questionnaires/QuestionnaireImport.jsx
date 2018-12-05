302a110968fd7d2ab255bf0e1cb51658aaae8cd5
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2018-12-05 15:33:28 -0300

    #1444. Apply styles in QuestionnaireImport component

--- web/static/js/components/questionnaires/QuestionnaireImport.jsx
+++ web/static/js/components/questionnaires/QuestionnaireImport.jsx
@@ -9,33 +9,41 @@ class QuestionnaireImport extends Component {
     const dashOffset = dashArray - dashArray * this.props.percentage / 100
 
     return (
-      <svg
-        id='questionnaireImport'
-        width={this.props.sqSize}
-        height={this.props.sqSize}
-        viewBox={viewBox}>
-        <path fill='#999999' transform='translate(12,12)' d='M75,0c41.4,0,75,33.6,75,75s-33.6,75-75,75S0,116.4,0,75S33.6,0,75,0L75,0z M48.4,106.9h52.4v-7.6H48.4V106.9z M85.8,91.8V69.3h14.9L74.6,43.1L48.4,69.3h14.9v22.5L85.8,91.8L85.8,91.8z' />
-        <circle
-          className='circle-progress'
-          cx={this.props.sqSize / 2}
-          cy={this.props.sqSize / 2}
-          r={radius}
-          strokeWidth={`${this.props.strokeWidth}px`}
-          // Start progress marker at 12 O'Clock
-          transform={`rotate(-90 ${this.props.sqSize / 2} ${this.props.sqSize / 2})`}
-          style={{
-            strokeDasharray: dashArray,
-            strokeDashoffset: dashOffset
-          }} />
-      </svg>
+      <div className='center-align'>
+        <svg
+          width={this.props.sqSize}
+          height={this.props.sqSize}
+          viewBox={viewBox}>
+          <path fill='#999999' transform='translate(12,12)' d='M75,0c41.4,0,75,33.6,75,75s-33.6,75-75,75S0,116.4,0,75S33.6,0,75,0L75,0z M48.4,106.9h52.4v-7.6H48.4V106.9z M85.8,91.8V69.3h14.9L74.6,43.1L48.4,69.3h14.9v22.5L85.8,91.8L85.8,91.8z' />
+          <circle
+            className='circle-progress'
+            cx={this.props.sqSize / 2}
+            cy={this.props.sqSize / 2}
+            r={radius}
+            strokeWidth={`${this.props.strokeWidth}px`}
+            // Start progress marker at 12 O'Clock
+            transform={`rotate(-90 ${this.props.sqSize / 2} ${this.props.sqSize / 2})`}
+            style={{
+              strokeDasharray: dashArray,
+              strokeDashoffset: dashOffset
+            }} />
+        </svg>
+        <h5 className='grey-text lighten-1'>
+          Uploading questionnaire
+        </h5>
+        <br />
+        <a href='#!' onClick={this.onCancel} className='btn-large red'>
+          Cancel
+        </a>
+      </div>
     )
   }
 }
 
 QuestionnaireImport.propTypes = {
-  percentage: PropTypes.number.isRequired,
-  sqSize: PropTypes.number.isRequired,
-  strokeWidth: PropTypes.strokeWidth.isRequired
+  percentage: PropTypes.number,
+  sqSize: PropTypes.number,
+  strokeWidth: PropTypes.number
 }
 
 export default QuestionnaireImport
