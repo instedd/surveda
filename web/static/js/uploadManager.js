d340d60499667f25de2279d9a9b37328b41e146f
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2018-12-13 15:02:59 -0300

    #1444. Restructure onReadyStateChange in uploadManager
    Delete uploads every time readyState is set to DONE. Also replaces random(100) with uuidv4 and add translation key for DEFAULT_ERROR_DESCRIPTION

--- web/static/js/uploadManager.js
+++ web/static/js/uploadManager.js
@@ -1,31 +1,34 @@
 // @flow
-import random from 'lodash/random'
+import uuidv4 from 'uuid/v4'
 
+const k = (...args: any) => args
 const uploads = {}
+const DEFAULT_ERROR_DESCRIPTION = k('Questionnaire upload failed')
 
 export const upload = (file: Object, url: string, onCompleted: ((response: Object) => mixed), onProgress: ((total: number, loaded: number) => mixed), onAbort: (() => mixed), onError: (description: ?string) => mixed) => {
   const formData = new FormData()
   formData.append('file', file)
   const xhr = new XMLHttpRequest()
-  const uploadId = random(100)
+  const uploadId = uuidv4()
 
   xhr.open('POST', url, true)
 
   xhr.onreadystatechange = function() {
-    // state XMLHttpRequest.DONE == 4
-    if (this.readyState === 4 && this.status === 200) {
+    // readyState is compared with 4 instead of DONE because the latter is not supported by all the browsers, and it is not compatible with 'flow'.
+    if (this.readyState === 4) {
       delete uploads[uploadId]
-      onCompleted(this.response)
-    } else if (this.status !== 200 && this.status !== 0) {
-      // Status is set to 0 when the request is cancelled.
-      // This is triggered, for example, when status changes to 400.
-      onError(this.statusText)
+      if (this.status === 200) {
+        onCompleted(this.response)
+      } else if (this.status !== 0) {
+        // Status is set to 0 when the request is cancelled, so we don't want to dispatch an error in this case
+        onError(this.statusText || DEFAULT_ERROR_DESCRIPTION)
+      }
     }
   }
 
   // onError contemplates the case when connection is lost
   xhr.onerror = function() {
-    const defaultDescription = 'Questionnaire upload failed'
+    const defaultDescription = DEFAULT_ERROR_DESCRIPTION
     onError(this.statusText || defaultDescription)
   }
 
@@ -45,6 +48,5 @@ export const upload = (file: Object, url: string, onCompleted: ((response: Objec
 export const cancel = (uploadId: number) => {
   if (uploads[uploadId]) {
     uploads[uploadId].abort()
-    delete uploads[uploadId]
   }
 }
