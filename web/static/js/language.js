74104e56951666057805eb55267213924e51288e
Hugo Farji <hugo.farji@gmail.com>
2019-03-22 15:12:38 -0300

    Added conditional to find alternative codes to some langs

--- web/static/js/language.js
+++ web/static/js/language.js
@@ -3,7 +3,7 @@ import iso6393 from 'iso-639-3'
 
 export function nameToCode(name: string): ?string {
   let match = iso6393.find(l => l.name == name)
-  return match ? match.iso6391 : null
+  return match ? (match.iso6391 || match.iso6393) : null
 }
 
 export function codeToName(code: string): ?string {
