4f153c67ef4f59b5759b47fd44271e3602a45175
devduarte <die.duarte@gmail.com>
2019-07-22 17:25:43 -0300

    Added new footer description

--- web/static/js/components/layout/Footer.jsx
+++ web/static/js/components/layout/Footer.jsx
@@ -6,10 +6,10 @@ import { translate } from 'react-i18next'
 export default translate()(({ t }) => (
   <footer>
     <div className='row'>
-      <div className='col s12 m2'>
-        InSTEDD ©
+      <div className='col s12 m7'>
+      InSTEDD© - GPLv3 - Developed in collaboration with Bloomberg Philanthropies, CDC, and RTI International.
       </div>
-      <div className='col s12 m10 right-align'>
+      <div className='col s12 m5 right-align'>
         <LanguageSelector className='languageSelector' />
         <span className='grey-text lighten2'>{t('Version: {{version}}', {version: config.version})}</span>
       </div>
