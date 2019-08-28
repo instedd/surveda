a52544c89741475b34c96458354d72befd508ca1
Matias Garcia Isaia <mgarcia@manas.tech>
2019-08-27 22:14:00 -0300

    Migrate old respondent hashed IDs to be prefixed
    See #1497

---
+++ priv/repo/migrations/20190826202301_prefix_respondent_hash_number.exs
@@ -0,0 +1,15 @@
+defmodule Ask.Repo.Migrations.PrefixRespondentHashNumber do
+  use Ecto.Migration
+
+  def up do
+    execute "UPDATE respondents SET hashed_number = CONCAT('r', hashed_number)"
+    execute "UPDATE respondent_disposition_history SET respondent_hashed_number = CONCAT('r', respondent_hashed_number)"
+    execute "UPDATE survey_log_entries SET respondent_hashed_number = CONCAT('r', respondent_hashed_number)"
+  end
+
+  def down do
+    execute "UPDATE survey_log_entries SET respondent_hashed_number = TRIM(LEADING 'r' FROM respondent_hashed_number)"
+    execute "UPDATE respondent_disposition_history SET respondent_hashed_number = TRIM(LEADING 'r' FROM respondent_hashed_number)"
+    execute "UPDATE respondents SET hashed_number = TRIM(LEADING 'r' FROM hashed_number)"
+  end
+end
