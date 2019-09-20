a50931693a0d69042804da67a0129faa6b1868c7
Andrés Atencio <aatencio@manas.tech>
2019-09-20 17:31:27 -0300

    Add attempts on stats
    For #1516

--- lib/ask/ecto_types/stats.ex
+++ lib/ask/ecto_types/stats.ex
@@ -2,7 +2,7 @@ defmodule Ask.Stats do
   @behaviour Ecto.Type
   alias __MODULE__
 
-  defstruct total_received_sms: 0, total_sent_sms: 0, total_call_time: 0
+  defstruct total_received_sms: 0, total_sent_sms: 0, total_call_time: 0, attempts: nil
 
   def type, do: :longtext
 
@@ -10,18 +10,20 @@ defmodule Ask.Stats do
     {:ok, stats}
   end
 
-  def cast(%{total_received_sms: total_received_sms, total_sent_sms: total_sent_sms, total_call_time: total_call_time}) do
+  def cast(%{total_received_sms: total_received_sms, total_sent_sms: total_sent_sms, total_call_time: total_call_time, attempts: attempts}) do
     {:ok, %Stats{
       total_received_sms: total_received_sms,
       total_sent_sms: total_sent_sms,
-      total_call_time: total_call_time
+      total_call_time: total_call_time,
+      attempts: attempts
     }}
   end
   def cast(%{} = map) do
     cast(%{
       total_received_sms: map["total_received_sms"],
       total_sent_sms: map["total_sent_sms"],
-      total_call_time: map["total_call_time"]
+      total_call_time: map["total_call_time"],
+      attempts: map["attempts"]
     })
   end
   def cast(nil), do: {:ok, %Stats{}}
@@ -54,5 +56,69 @@ defmodule Ask.Stats do
 
   def add_sent_sms(%Stats{total_sent_sms: total} = stats, count \\ 1) do
     %{stats | total_sent_sms: total + count}
+  end
+
+  def attempts(%Stats{attempts: nil }, :sms) do
+    0
+  end
+  def attempts(%Stats{attempts: %{ sms: total }}, :sms) do
+    total
+  end
+  def attempts(%Stats{attempts: _}, :sms) do
+    0
+  end
+
+  def attempts(%Stats{attempts: nil }, :ivr) do
+    0
+  end
+  def attempts(%Stats{attempts: %{ ivr: total }}, :ivr) do
+    total
+  end
+  def attempts(%Stats{attempts: _}, :ivr) do
+    0
+  end
+
+  def attempts(%Stats{attempts: nil }, :mobileweb) do
+    0
+  end
+  def attempts(%Stats{attempts: %{ mobileweb: total }}, :mobileweb) do
+    total
+  end
+  def attempts(%Stats{attempts: _}, :mobileweb) do
+    0
+  end
+
+  def attempts(%Stats{} = stats, :all) do
+    attempts(stats, :sms) + attempts(stats, :ivr) + attempts(stats, :mobileweb)
+  end
+
+  def add_attempt(%Stats{attempts: nil} = stats, :sms) do
+    %{stats | attempts: %{ sms: 1 }}
+  end
+  def add_attempt(%Stats{attempts: %{ sms: total } = attempts } = stats, :sms) do
+    %{stats | attempts: %{attempts | sms: total + 1}}
+  end
+  def add_attempt(%Stats{attempts: attempts} = stats, :sms) do
+    %{stats | attempts: Map.put(attempts, :sms, 1)}
+  end
+
+  def add_attempt(%Stats{attempts: nil} = stats, :ivr) do
+    %{stats | attempts: %{ ivr: 1 }}
+  end
+  def add_attempt(%Stats{attempts: %{ ivr: total } = attempts } = stats, :ivr) do
+    %{stats | attempts: %{attempts | ivr: total + 1}}
+  end
+  def add_attempt(%Stats{attempts: attempts} = stats, :ivr) do
+    %{stats | attempts: Map.put(attempts, :ivr, 1)}
+  end
+
+  def add_attempt(%Stats{attempts: nil} = stats, :mobileweb) do
+    %{stats | attempts: %{ mobileweb: 1 }}
+  end
+  def add_attempt(%Stats{attempts: %{ mobileweb: total } = attempts } = stats, :mobileweb) do
+    %{stats | attempts: %{attempts | mobileweb: total + 1}}
+  end
+  def add_attempt(%Stats{attempts: attempts} = stats, :mobileweb) do
+    %{stats | attempts: Map.put(attempts, :mobileweb, 1)}
   end
 end
--- test/lib/ecto_types/stats_test.exs
+++ test/lib/ecto_types/stats_test.exs
@@ -4,31 +4,146 @@ defmodule Ask.StatsTest do
 
   describe "dump:" do
     test "should dump empty" do
-      assert {:ok, "{\"total_sent_sms\":0,\"total_received_sms\":0,\"total_call_time\":0}"} == Stats.dump(%Stats{})
+      assert {:ok, "{\"total_sent_sms\":0,\"total_received_sms\":0,\"total_call_time\":0,\"attempts\":null}"} == Stats.dump(%Stats{})
     end
 
     test "should dump full" do
-      assert {:ok, "{\"total_sent_sms\":3,\"total_received_sms\":2,\"total_call_time\":1}"} == Stats.dump(%Stats{total_received_sms: 2, total_sent_sms: 3, total_call_time: 1})
+      assert {:ok, "{\"total_sent_sms\":3,\"total_received_sms\":2,\"total_call_time\":1,\"attempts\":{\"sms\":5,\"mobileweb\":7,\"ivr\":6}}"} == Stats.dump(%Stats{total_received_sms: 2, total_sent_sms: 3, total_call_time: 1, attempts: %{sms: 5, ivr: 6, mobileweb: 7}})
     end
   end
 
   describe "load:" do
     test "should load empty" do
-      assert {:ok, %Stats{total_received_sms: 0, total_sent_sms: 0, total_call_time: 0}} == Stats.load("{\"total_sent_sms\":0,\"total_received_sms\":0,\"total_call_time\":0}")
+      assert {:ok, %Stats{total_received_sms: 0, total_sent_sms: 0, total_call_time: 0, attempts: nil}} == Stats.load("{\"total_sent_sms\":0,\"total_received_sms\":0,\"total_call_time\":0}")
+      assert {:ok, %Stats{total_received_sms: 0, total_sent_sms: 0, total_call_time: 0, attempts: nil}} == Stats.load("{\"total_sent_sms\":0,\"total_received_sms\":0,\"total_call_time\":0,\"attempts\":null}")
     end
 
     test "should load full" do
-      assert {:ok, %Stats{total_received_sms: 2, total_sent_sms: 3, total_call_time: 1}} == Stats.load("{\"total_sent_sms\":3,\"total_received_sms\":2,\"total_call_time\":1}")
+      assert {:ok, %Stats{total_received_sms: 2, total_sent_sms: 3, total_call_time: 1, attempts: %{"ivr" => 6, "sms" => 5, "mobileweb" => 7}}} == Stats.load("{\"total_sent_sms\":3,\"total_received_sms\":2,\"total_call_time\":1,\"attempts\":{\"mobileweb\":7,\"sms\":5,\"ivr\":6}}")
     end
   end
 
   describe "cast:" do
     test "shuld cast to itself" do
       assert {:ok, %Stats{total_received_sms: 2, total_sent_sms: 3, total_call_time: 1}} == Stats.cast(%Stats{total_received_sms: 2, total_sent_sms: 3, total_call_time: 1})
+      assert {:ok, %Stats{total_received_sms: 2, total_sent_sms: 3, total_call_time: 1, attempts: %{sms: 5, ivr: 6, mobileweb: 7}}} == Stats.cast(%Stats{total_received_sms: 2, total_sent_sms: 3, total_call_time: 1, attempts: %{sms: 5, ivr: 6, mobileweb: 7}})
     end
 
     test "shuld cast nil" do
       assert {:ok, %Stats{}} == Stats.cast(nil)
+    end
+  end
+
+  describe "mode attempts" do
+    test "adds sms" do
+      stats = %Stats{}
+      assert 0 == stats |> Stats.attempts(:sms)
+      assert 0 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:sms)
+      assert 1 == stats |> Stats.attempts(:sms)
+      assert 1 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:sms)
+      assert 2 == stats |> Stats.attempts(:sms)
+      assert 2 == stats |> Stats.attempts(:all)
+
+      assert 0 == stats |> Stats.attempts(:ivr)
+      assert 0 == stats |> Stats.attempts(:mobileweb)
+      assert 2 == stats |> Stats.attempts(:all)
+    end
+
+    test "adds ivr" do
+      stats = %Stats{}
+      assert 0 == stats |> Stats.attempts(:ivr)
+      assert 0 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:ivr)
+      assert 1 == stats |> Stats.attempts(:ivr)
+      assert 1 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:ivr)
+      assert 2 == stats |> Stats.attempts(:ivr)
+      assert 2 == stats |> Stats.attempts(:all)
+
+      assert 0 == stats |> Stats.attempts(:sms)
+      assert 0 == stats |> Stats.attempts(:mobileweb)
+      assert 2 == stats |> Stats.attempts(:all)
+    end
+
+    test "adds mobileweb" do
+      stats = %Stats{}
+      assert 0 == stats |> Stats.attempts(:mobileweb)
+      assert 0 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:mobileweb)
+      assert 1 == stats |> Stats.attempts(:mobileweb)
+      assert 1 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:mobileweb)
+      assert 2 == stats |> Stats.attempts(:mobileweb)
+      assert 2 == stats |> Stats.attempts(:all)
+
+      assert 0 == stats |> Stats.attempts(:sms)
+      assert 0 == stats |> Stats.attempts(:ivr)
+      assert 2 == stats |> Stats.attempts(:all)
+    end
+
+    test "adds multiple modes" do
+      stats = %Stats{}
+
+      assert 0 == stats |> Stats.attempts(:sms)
+      assert 0 == stats |> Stats.attempts(:ivr)
+      assert 0 == stats |> Stats.attempts(:mobileweb)
+      assert 0 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:sms)
+      assert 1 == stats |> Stats.attempts(:sms)
+      assert 1 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:ivr)
+      assert 1 == stats |> Stats.attempts(:ivr)
+      assert 2 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:mobileweb)
+      assert 1 == stats |> Stats.attempts(:mobileweb)
+      assert 3 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:sms)
+      assert 2 == stats |> Stats.attempts(:sms)
+      assert 4 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:ivr)
+      assert 2 == stats |> Stats.attempts(:ivr)
+      assert 5 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:mobileweb)
+      assert 2 == stats |> Stats.attempts(:mobileweb)
+      assert 6 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:ivr)
+      assert 3 == stats |> Stats.attempts(:ivr)
+      assert 7 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:mobileweb)
+      assert 3 == stats |> Stats.attempts(:mobileweb)
+      assert 8 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:mobileweb)
+      assert 4 == stats |> Stats.attempts(:mobileweb)
+      assert 9 == stats |> Stats.attempts(:all)
+
+      stats = %Stats{}
+      assert 0 == stats |> Stats.attempts(:mobileweb)
+      assert 0 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:mobileweb)
+      assert 1 == stats |> Stats.attempts(:mobileweb)
+      assert 1 == stats |> Stats.attempts(:all)
+
+      stats = stats |> Stats.add_attempt(:sms)
+      assert 1 == stats |> Stats.attempts(:sms)
+      assert 2 == stats |> Stats.attempts(:all)
     end
   end
 
