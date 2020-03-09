040453e377ecf4b85641963e2c50b16b56e98baf
aeatencio <aeatencio@gmail.com>
2020-03-09 11:09:49 -0300

    Make respondent.user_stopped a required field
    Also, centralize the default responsability in the model side
    
    For #1607

--- priv/repo/migrations/20200212200209_add-user-stopped-to-respondents.exs
+++ priv/repo/migrations/20200212200209_add-user-stopped-to-respondents.exs
@@ -1,9 +1,15 @@
 defmodule :"Elixir.Ask.Repo.Migrations.Add-user-stopped-to-respondents" do
   use Ecto.Migration
 
-  def change do
-    alter table(:respondents) do
-      add :user_stopped, :boolean, default: false
-    end
+  def up do
+    alter table(:respondents), do:
+      add :user_stopped, :boolean, null: false, default: false
+    alter table(:respondents), do:
+      modify :user_stopped, :boolean, null: false
+  end
+
+  def down do
+    alter table(:respondents), do:
+      remove :user_stopped
   end
 end
