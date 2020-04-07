4d9de8cf0a95be44a760193f746b2ceb44a82695
Ana Perez Ghiglia <aghiglia@manas.tech>
2020-04-07 11:31:59 -0300

    define necessary QuestionnaireSimulator iteraction with Session and Survey

--- lib/ask/runtime/questionnaire_simulator.ex
+++ lib/ask/runtime/questionnaire_simulator.ex
@@ -1,11 +1,12 @@
 defmodule Ask.QuestionnaireSimulation do
-  defstruct [:respondent, :questionnaire, :survey]
+  defstruct [:respondent, :questionnaire, :survey, :session]
 end
 
 defmodule Ask.QuestionnaireSimulator do
   use Agent
 
-  alias Ask.{Survey, Respondent, Questionnaire, Project}
+  alias Ask.{Survey, Respondent, Questionnaire, Project, SystemTime}
+  alias Ask.Runtime.{Survey, Session, Flow}
 
   def start_link() do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
@@ -34,10 +35,29 @@ defmodule Ask.QuestionnaireSimulator do
 
     respondent = %Respondent{
       id: Ecto.UUID.generate(),
-      survey_id: survey.id
-    }
+      survey_id: survey.id,
+      questionnaire_id: questionnaire.id,
+      mode: [mode],
+      disposition: "queued"}
 
-    Ask.QuestionnaireSimulator.add_respondent_simulation(respondent.id, %Ask.QuestionnaireSimulation{survey: survey, questionnaire: questionnaire, respondent: respondent})
+    session = Session.start(questionnaire, respondent, nil, mode, Ask.Schedule.always(), [], nil, nil, [], nil, false)
+              |> Survey.handle_session_step(SystemTime.time.now, false)
+
+    Ask.QuestionnaireSimulator.add_respondent_simulation(respondent.id, %Ask.QuestionnaireSimulation{survey: survey, questionnaire: questionnaire, respondent: respondent, session: session})
+    session
+  end
+
+  def process_respondent_response(respondent_id, response) do
+    simulation = Ask.QuestionnaireSimulator.get_respondent_status(respondent_id)
+    respondent = simulation |> prepare_respondent
+    reply = Flow.Message.reply(response)
+    session = Survey.sync_step(respondent, reply, nil, SystemTime.time.now, false)
+    Ask.QuestionnaireSimulator.add_respondent_simulation(respondent.id, %Ask.QuestionnaireSimulation{simulation | session:  session})
+    session
+  end
+
+  defp prepare_respondent(simulation) do
+    %Respondent{simulation.respondent | session: Session.dump(simulation.session)}
   end
 end
 
