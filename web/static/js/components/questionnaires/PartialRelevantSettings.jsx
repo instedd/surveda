9f8aa1f3e0beffb013507c495f4658c2c478d4c2
aeatencio <aeatencio@gmail.com>
2020-04-28 12:39:35 -0300

    Erase every invalid min relevant steps value
    Before this commit, it was being erased only the first time
    
    For #1691

--- web/static/js/components/questionnaires/PartialRelevantSettings.jsx
+++ web/static/js/components/questionnaires/PartialRelevantSettings.jsx
@@ -4,6 +4,11 @@ import Draft from './Draft'
 import { translate } from 'react-i18next'
 import { Card } from '../ui'
 
+type State = {
+  minRelevantSteps: ?number,
+  minRelevantStepsKey: number
+}
+
 type Props = {
   t: Function,
   readOnly: boolean,
@@ -15,9 +20,24 @@ type Props = {
   relevantStepsQuantity: number
 }
 
-class PartialRelevantSettings extends Component<Props> {
+class PartialRelevantSettings extends Component<Props, State> {
+
+  constructor(props) {
+    super(props)
+    this.state = this.stateFromProps(props)
+  }
+
+  stateFromProps(props) {
+    const { minRelevantSteps } = props
+    return {
+      minRelevantSteps,
+      minRelevantStepsKey: 0
+    }
+  }
+
   handleMinRelevantStepsOnBlur(changed) {
     const { changeMinRelevantSteps } = this.props
+    const { minRelevantSteps, minRelevantStepsKey } = this.state
     let nextValue = null
     if (changed) {
       const onlyNumbers = /^\d+$/.test(changed)
@@ -27,6 +47,9 @@ class PartialRelevantSettings extends Component<Props> {
           nextValue = parsed
         }
       }
+    }
+    if (!minRelevantSteps && !nextValue) {
+      this.setState({ minRelevantSteps: nextValue, minRelevantStepsKey: minRelevantStepsKey + 1 })
     }
     changeMinRelevantSteps(nextValue)
   }
@@ -49,10 +72,12 @@ class PartialRelevantSettings extends Component<Props> {
   }
 
   minRelevantStepsComponent() {
-    const { minRelevantSteps, readOnly, t } = this.props
+    const { readOnly, t } = this.props
+    const { minRelevantSteps, minRelevantStepsKey } = this.state
 
     return <Draft
       id='partial_relevant_min_relevant_steps'
+      key={minRelevantStepsKey}
       value={(minRelevantSteps || '').toString()}
       label={t('Min Relevant Steps')}
       textBelow={`out of ${this.props.relevantStepsQuantity} relevant questions selected`}
