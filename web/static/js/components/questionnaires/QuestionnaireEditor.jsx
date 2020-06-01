6f2d881e5a9fd4a5738e4161e7a0e060654b7e1e
Dani Mibashan <dmibashan@manas.com.ar>
2020-06-01 15:33:06 -0300

    Fix lint issues

--- web/static/js/components/questionnaires/QuestionnaireEditor.jsx
+++ web/static/js/components/questionnaires/QuestionnaireEditor.jsx
@@ -280,7 +280,7 @@ class QuestionnaireEditor extends Component<any, State> {
   }
 
   render() {
-    const { questionnaire,  questionnaireHasSections, errors, project, readOnly, userSettings, errorsByPath, selectedSteps, selectedQuotaCompletedSteps, uploadId, uploadError, uploadProgress, userLevel, t } = this.props
+    const { questionnaire, questionnaireHasSections, errors, project, readOnly, userSettings, errorsByPath, selectedSteps, selectedQuotaCompletedSteps, uploadId, uploadError, uploadProgress, userLevel, t } = this.props
 
     if (questionnaire == null || project == null || userSettings.settings == null) {
       return <div>Loading...</div>
@@ -295,7 +295,7 @@ class QuestionnaireEditor extends Component<any, State> {
     const settings = userSettings.settings
     // emptyQuestionnaire is to show onboarding to fresh users with write level in new or empty questionnaires. As questionnaires are created with an empty step, all this conditions are necessary to check if it is actually empty
     const emptyChoices = questionnaire.steps[0].choices && questionnaire.steps[0].choices.length == 0
-    const emptyQuestionnaire = !questionnaireHasSections && !questionnaire.name && questionnaire.steps.length == 1 && questionnaire.steps[0].title == "" && emptyChoices
+    const emptyQuestionnaire = !questionnaireHasSections && !questionnaire.name && questionnaire.steps.length == 1 && questionnaire.steps[0].title == '' && emptyChoices
     const skipOnboarding = !emptyQuestionnaire || (settings.onboarding && settings.onboarding.questionnaire) || userLevel == 'reader'
     const hasQuotaCompletedSteps = !!questionnaire.quotaCompletedSteps
     const partialRelevantEnabled = questionnaire.partialRelevantConfig && questionnaire.partialRelevantConfig.enabled
