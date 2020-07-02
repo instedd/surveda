8b427fc94a5c12d8db6a5dace18deb118deaf5ae
Andrés Atencio <aatencio@manas.tech>
2020-07-01 21:25:55 -0300

    Cancel debounced request on Enter
    Pending: discard result if the input changed after last request
    
    For #1725

--- web/static/js/components/respondents/RespondentsFilter.jsx
+++ web/static/js/components/respondents/RespondentsFilter.jsx
@@ -1,7 +1,6 @@
 // @flow
 import React, { Component } from 'react'
 import { translate } from 'react-i18next'
-import AwesomeDebouncePromise from 'awesome-debounce-promise'
 
 type Props = {
   t: Function,
@@ -11,34 +10,45 @@ type Props = {
 }
 
 type State = {
-  inputValue: string,
-  applyFilterDebounced: Function
+  inputValue: string
 }
 
 class RespondentsFilter extends Component<Props, State> {
+  _timeoutId = null
+
   constructor(props) {
     super(props)
     this.state = {
-      inputValue: props.inputValue,
-      applyFilterDebounced: AwesomeDebouncePromise(
-        props.onApplyFilter,
-        500
-      )
+      inputValue: props.inputValue
     }
   }
 
   onInputChange = (event) => {
     const { onChange } = this.props
-    const { applyFilterDebounced } = this.state
     const inputValue = event.target.value
     this.setState({ inputValue: inputValue })
     onChange(inputValue)
-    applyFilterDebounced()
+    this.applyFilter({ debounced: true })
   }
 
   onInputKeyPress = (event) => {
+    if (event.key === 'Enter') this.applyFilter()
+  }
+
+  applyFilter = ({ debounced } = { debounced: false }) => {
     const { onApplyFilter } = this.props
-    if (event.key === 'Enter') onApplyFilter()
+    if (this._timeoutId != null) {
+      clearTimeout(this._timeoutId)
+      this._timeoutId = null
+    }
+    if (debounced) {
+      this._timeoutId = setTimeout(() => {
+        this._timeoutId = null
+        onApplyFilter()
+      }, 500)
+    } else {
+      onApplyFilter()
+    }
   }
 
   render = () => {
