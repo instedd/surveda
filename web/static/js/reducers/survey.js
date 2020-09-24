924845427e8d92f9af8b9f202388a122d2ae3c04
aeatencio <aeatencio@gmail.com>
2020-09-24 09:38:07 -0300

    Make it easier for future modifications
    See https://github.com/instedd/surveda/pull/1781#issuecomment-697364729
    
    For #1095

--- web/static/js/reducers/survey.js
+++ web/static/js/reducers/survey.js
@@ -97,7 +97,7 @@ const validateRetry = (state: DataStore<Survey>, mode, key) => {
 
   let values = retriesValue.split(' ')
   values = values.filter((v) => v)
-  const invalid = values.some((v) => !timeSpecRegex().test(v))
+  const invalid = values.some((recontactTime) => !isValidReContactTime(recontactTime))
   if (invalid) {
     state.errorsByPath[key] = [k('Re-contact configuration is invalid')]
   }
@@ -117,23 +117,26 @@ const validateFallbackDelay = (state: DataStore<Survey>) => {
   const fallbackDelay = data.fallbackDelay
   if (!fallbackDelay) return
 
-  const invalid = !timeSpecRegex().test(fallbackDelay)
+  const invalid = !isValidReContactTime(fallbackDelay)
   if (invalid) {
     state.errorsByPath.fallbackDelay = [k('Fallback delay is invalid')]
   }
 }
 
 /**
- * Attempts are at least every 10m
+ * Recontacts are at least every 10m
  * So the minimum by:
  *  - Days: 1d
  *  - Hours: 1h
  *  - Minutes: 10m
  */
-const timeSpecRegex = () => {
-  const atLeast1 = '[1-9]\\d*'
-  const atLeast10 = '[1-9]\\d+'
-  return new RegExp(`^(${atLeast10}m)|(${atLeast1}[hd])$`)
+const isValidReContactTime = (recontactTime) => {
+  const matches = recontactTime.match(/^(\d+)([hdm])$/)
+  if (matches == null) return false
+  const [quantity, unit] = [parseInt(matches[1]), matches[2]]
+  if (quantity == 0) return false
+  if (unit == 'm' && quantity < 10) return false
+  return true
 }
 
 const changeName = (state, action) => {
