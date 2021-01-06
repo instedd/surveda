ca5be6c3f99d82e31dd11b9f09cf4940ac7dd9e0
Mauricio <mmuller@manas.com.ar>
2021-01-06 18:10:53 -0300

    Complete list of state and disposition combinations

--- integration_tests/cypress/support/commands.js
+++ integration_tests/cypress/support/commands.js
@@ -89,11 +89,29 @@ Cypress.Commands.add('waitForUrl', (pattern) => {
 
 function validRespondentStateDisposition(respondent) {
   const validStateDisposition = [
+    { disposition: 'contacted', state: 'active' },
+    { disposition: 'interim partial', state: 'active' },
+    { disposition: 'queued', state: 'active' },
+    { disposition: 'started', state: 'active' },
+    { disposition: 'breakoff', state: 'cancelled' },
     { disposition: 'contacted', state: 'cancelled' },
+    { disposition: 'failed', state: 'cancelled' },
+    { disposition: 'inelegible', state: 'cancelled' },
+    { disposition: 'interim partial', state: 'cancelled' },
+    { disposition: 'queued', state: 'cancelled' },
+    { disposition: 'refused', state: 'cancelled' },
     { disposition: 'started', state: 'cancelled' },
+    { disposition: 'unresponsive', state: 'cancelled' },
+    { disposition: 'completed', state: 'completed' },
+    { disposition: 'ineligible', state: 'completed' },
+    { disposition: 'partial', state: 'completed' },
+    { disposition: 'refused', state: 'completed' },
+    { disposition: 'rejected', state: 'completed' },
+    { disposition: 'breakoff', state: 'failed' }, 
+    { disposition: 'failed', state: 'failed' }, 
+    { disposition: 'unresponsive', state: 'failed' }, 
+    { disposition: 'registered', state: 'pending' }, 
     { disposition: 'rejected', state: 'rejected' },
-
-    // TODO add valid combinations
   ]
   return !!validStateDisposition.find(x => x.disposition == respondent.disposition && x.state == respondent.state)
 }
