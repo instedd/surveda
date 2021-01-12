2d87cbf12b1c92505b2de68bb6169f84f4ec62c5
aeatencio <aeatencio@gmail.com>
2021-01-12 16:32:39 -0300

    Bring the Stats component
    From https://github.com/instedd/surveda-d3-components/commit/862f6f0a43639a78f7bff4aabb7807c00b16906a
    
    For #1807

---
+++ web/static/js/components/d3/Stats.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import * as d3 from 'd3-4'
+
+const formatter = d3.format(',')
+
+type Props = {
+  data: Array<Object>
+}
+
+export default class Stats extends Component<Props> {
+  render() {
+    const {data} = this.props
+
+    return (
+      <div className='stats'>
+        {
+
+          data.map((item, index) => {
+            return (
+              <div key={index}>{formatter(item.value)}<span className='label'>{item.label}</span></div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
