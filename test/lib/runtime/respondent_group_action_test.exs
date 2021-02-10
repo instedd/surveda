47e00bc91bc991386d559ecc9ce0610e30b4b6f8
aeatencio <aeatencio@gmail.com>
2021-02-10 12:00:42 -0300

    Make it easier to read

--- test/lib/runtime/respondent_group_action_test.exs
+++ test/lib/runtime/respondent_group_action_test.exs
@@ -9,15 +9,19 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
 
   describe "load_entries" do
     test "loads a phone_number", %{survey: survey} do
+      # Arrange
       [phone_number] = entries = ["1000000001"]
 
+      # Act
       {result, loaded_entries} = RespondentGroupAction.load_entries(entries, survey)
 
+      # Assert
       assert result == :ok
       assert loaded_entries == [%{phone_number: phone_number}]
     end
 
     test "loads an existent respondent_id", %{survey: survey} do
+      # Arrange
       other_survey = insert(:survey, project: survey.project)
       phone_number = "1000000001"
 
@@ -27,16 +31,18 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
 
       entries = [respondent_id]
 
+      # Act
       {result, loaded_entries} = RespondentGroupAction.load_entries(entries, survey)
 
+      # Assert
       assert result == :ok
       assert loaded_entries == [%{phone_number: phone_number, hashed_number: respondent_id}]
     end
 
     test "loads a phone_number and an existent respondent_id together", %{survey: survey} do
+      # Arrange
       phone_number_1 = "1000000001"
       phone_number_2 = "1000000002"
-
       other_survey = insert(:survey, project: survey.project)
 
       respondent_id =
@@ -45,8 +51,10 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
 
       entries = [respondent_id, phone_number_2]
 
+      # Act
       {result, loaded_entries} = RespondentGroupAction.load_entries(entries, survey)
 
+      # Assert
       assert result == :ok
 
       assert loaded_entries == [
@@ -56,19 +64,25 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
     end
 
     test "validates an invalid entry", %{survey: survey} do
+      # Arrange
       [entry] = entries = ["foo"]
 
+      # Act
       {result, invalid_entries} = RespondentGroupAction.load_entries(entries, survey)
 
+      # Assert
       assert result == :error
       assert invalid_entries == [%{entry: entry, line_number: 1}]
     end
 
     test "validates a non-existent respondent_id", %{survey: survey} do
+      # Arrange
       [respondent_id] = entries = ["r000000000001"]
 
+      # Act
       {result, invalid_entries} = RespondentGroupAction.load_entries(entries, survey)
 
+      # Assert
       assert result == :error
       assert invalid_entries == [%{entry: respondent_id, line_number: 1, type: "not-found"}]
     end
@@ -76,6 +90,7 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
     test "validates a non-existent respondent_id (but existent in another project)", %{
       survey: survey
     } do
+      # Arrange
       other_survey = insert(:survey)
 
       respondent_id =
@@ -84,8 +99,10 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
 
       entries = [respondent_id]
 
+      # Act
       {result, invalid_entries} = RespondentGroupAction.load_entries(entries, survey)
 
+      # Assert
       assert result == :error
       assert invalid_entries == [%{entry: respondent_id, line_number: 1, type: "not-found"}]
     end
@@ -93,6 +110,7 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
 
   describe "create" do
     test "creates a new respondent from an existent respondent_id", %{survey: survey} do
+      # Arrange
       phone_number = "1000000001"
       other_survey = insert(:survey, project: survey.project)
 
@@ -102,8 +120,10 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
 
       {:ok, loaded_entries} = RespondentGroupAction.load_entries([respondent_id], survey)
 
+      # Act
       respondent_group = RespondentGroupAction.create("foo", loaded_entries, survey)
 
+      # Assert
       assert respondent_group.sample == [respondent_id]
 
       assert Repo.get_by(Respondent,
@@ -117,12 +137,11 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
   describe "add_respondents" do
     test "creates a new respondent from an existent respondent_id into a specific respondent group",
          %{survey: survey} do
+      # Arrange
       phone_number_1 = "1000000001"
       phone_number_2 = "1000000002"
       other_survey = insert(:survey, project: survey.project)
-
       respondent_group = create_respondent_group([phone_number_1], survey)
-
       respondent_id_1 = first_respondent_id(respondent_group)
 
       respondent_id_2 =
@@ -131,9 +150,11 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
 
       {:ok, loaded_entries} = RespondentGroupAction.load_entries([respondent_id_2], survey)
 
+      # Act
       respondent_group =
         RespondentGroupAction.add_respondents(respondent_group, loaded_entries, "bar", nil)
 
+      # Assert
       assert respondent_group.sample == [phone_number_1, respondent_id_2]
 
       assert Repo.all(
@@ -148,10 +169,10 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
   describe "replace_respondents" do
     test "creates a new respondent from an existent respondent_id and replace the respondent group",
          %{survey: survey} do
+      # Arrange
       phone_number_1 = "1000000001"
       phone_number_2 = "1000000002"
       other_survey = insert(:survey, project: survey.project)
-
       respondent_group = create_respondent_group([phone_number_1], survey)
 
       respondent_id =
@@ -160,9 +181,11 @@ defmodule Ask.Runtime.RespondentGroupActionTest do
 
       {:ok, loaded_entries} = RespondentGroupAction.load_entries([respondent_id], survey)
 
+      # Act
       respondent_group =
         RespondentGroupAction.replace_respondents(respondent_group, loaded_entries)
 
+      # Assert
       assert respondent_group.sample == [respondent_id]
 
       assert Repo.all(
