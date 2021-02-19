712c0b79e1837002c61953c77152282658d9d7bd
aeatencio <aeatencio@gmail.com>
2021-02-19 10:37:29 -0300

    Display the incentive disabled warning immediately
    After uploading the CSV
    
    For #1834

--- web/views/respondent_group_view.ex
+++ web/views/respondent_group_view.ex
@@ -1,7 +1,7 @@
 defmodule Ask.RespondentGroupView do
   use Ask.Web, :view
 
-  alias Ask.Respondent
+  alias Ask.{Respondent, Repo}
 
   def render("index.json", %{respondent_groups: respondent_groups}) do
     %{data: (respondent_groups |> Enum.map(fn respondent_group ->
@@ -14,6 +14,7 @@ defmodule Ask.RespondentGroupView do
   end
 
   def render("respondent_group.json", %{respondent_group: respondent_group}) do
+    respondent_group = Repo.preload(respondent_group, :survey)
     sample = respondent_group.sample |> Enum.map(&Respondent.mask_respondent_entry/1)
 
     channels = respondent_group.respondent_group_channels
@@ -25,6 +26,9 @@ defmodule Ask.RespondentGroupView do
       sample: sample,
       respondents_count: respondent_group.respondents_count,
       channels: channels,
+      # TODO: presenting this survey flag in the respondent group is a workaround
+      # In the future we should handle the survey / respondent_group UI updates better
+      incentives_enabled: respondent_group.survey.incentives_enabled
     }
   end
 
