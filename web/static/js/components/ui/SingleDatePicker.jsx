4ec11677b52add3d8c8f778d314b5f7a39bb3d32
aeatencio <aeatencio@gmail.com>
2021-03-01 12:01:20 -0300

    Styling and i18n
    For #1838

--- web/static/js/components/ui/SingleDatePicker.jsx
+++ web/static/js/components/ui/SingleDatePicker.jsx
@@ -2,6 +2,7 @@
 import { Card } from '../ui'
 import InfiniteCalendar from 'react-infinite-calendar'
 import React, { Component } from 'react'
+import i18n from '../../i18next'
 
 type Props = {
   readOnly: boolean,
@@ -13,11 +14,57 @@ type State = {
   showDatePicker: boolean
 }
 
+const enLocale = {
+  name: 'en',
+  blank: 'Select a date...',
+  headerFormat: 'ddd, MMM Do',
+  todayLabel: {
+    long: 'Today'
+  },
+  weekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  weekStartsOn: 0
+}
+
+const esLocale = {
+  name: 'es',
+  blank: 'Elija una fecha...',
+  headerFormat: 'ddd, MMM Do',
+  locale: require('date-fns/locale/es'),
+  todayLabel: {
+    long: 'Hoy'
+  },
+  weekdays: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+  weekStartsOn: 0
+}
+
+const frLocale = {
+  name: 'fr',
+  blank: 'Aucune date sélectionnée',
+  headerFormat: 'dddd, D MMM',
+  locale: require('date-fns/locale/fr'),
+  todayLabel: {
+    long: "Aujourd'hui",
+    short: 'Auj.'
+  },
+  weekdays: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+  weekStartsOn: 1
+}
 export default class SingleDatePicker extends Component<Props, State> {
   constructor(props: Props) {
     super(props)
     this.state = {
       showDatePicker: false
+    }
+  }
+
+  getLocale(locale: string) {
+    switch (locale) {
+      case 'en':
+        return enLocale
+      case 'fr':
+        return frLocale
+      case 'es':
+        return esLocale
     }
   }
 
@@ -39,7 +86,32 @@ export default class SingleDatePicker extends Component<Props, State> {
       {
         this.state.showDatePicker
           ? <Card className='datepicker-card'>
-            <InfiniteCalendar selected={selected} onSelect={onSelect} />
+            <InfiniteCalendar selected={selected} onSelect={onSelect}
+              theme={{
+                accentColor: '#4CAF50',
+                floatingNav: {
+                  background: 'rgba(245, 245, 245, 0.94)',
+                  chevron: '#9a9a9a',
+                  color: '#9a9a9a'
+                },
+                headerColor: '#FFF',
+                selectionColor: '#FFF',
+                textColor: {
+                  active: '#9a9a9a',
+                  default: '#333'
+                },
+                todayColor: '#e0e0e0',
+                weekdayColor: '#FFF'
+              }}
+              displayOptions={{
+                showHeader: true,
+                showWeekdays: true
+              }}
+              locale={this.getLocale(i18n.language)}
+              width='100%'
+              displayDate={false}
+              height={300}
+             />
           </Card>
         : null
       }
