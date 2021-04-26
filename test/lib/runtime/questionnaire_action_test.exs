2199990c3708d902ebd546fff3d6151b23efd8fa
aeatencio <aeatencio@gmail.com>
2021-04-26 17:58:50 -0300

    Fix export when choices responses
    Under certain conditions it crashes
    Because of a bug in the clean algorithm
    
    For #1868

--- test/lib/runtime/questionnaire_action_test.exs
+++ test/lib/runtime/questionnaire_action_test.exs
@@ -20,8 +20,8 @@ defmodule Ask.Runtime.QuestionnaireExportTest do
     end
   end
 
-  describe "CleanI18n" do
-    test "base case" do
+  describe "CleanI18n.clean/3" do
+    test "cleans a base case" do
       entity = %{"en" => "foo", "es" => "bar"}
 
       clean = CleanI18n.clean(entity, ["en"], "")
@@ -29,7 +29,7 @@ defmodule Ask.Runtime.QuestionnaireExportTest do
       assert clean == %{"en" => "foo"}
     end
 
-    test "clean every map element" do
+    test "cleans every map element" do
       entity = %{"bar" => %{"en" => "foo", "es" => "bar"}}
 
       clean = CleanI18n.clean(entity, ["en"], ".[]")
@@ -37,7 +37,7 @@ defmodule Ask.Runtime.QuestionnaireExportTest do
       assert clean == %{"bar" => %{"en" => "foo"}}
     end
 
-    test "clean every list element" do
+    test "cleans every list element" do
       entity = [%{"en" => "foo", "es" => "bar"}]
 
       clean = CleanI18n.clean(entity, ["en"], ".[]")
@@ -45,12 +45,45 @@ defmodule Ask.Runtime.QuestionnaireExportTest do
       assert clean == [%{"en" => "foo"}]
     end
 
-    test "clean the requested key of a map" do
+    test "cleans the requested key of a map" do
       entity = %{"a" => %{"en" => "foo", "es" => "bar"}, "b" => %{"en" => "foo", "es" => "bar"}}
 
       clean = CleanI18n.clean(entity, ["en"], ".a")
 
       assert clean == %{"a" => %{"en" => "foo"}, "b" => %{"en" => "foo", "es" => "bar"}}
+    end
+
+    test "doesn't crash when the content of the requested key isn't a map" do
+      # A real case that was making it crash:
+      # entity = [
+      #   %{
+      #     "choices" => [
+      #       %{
+      #         "responses" => %{"ivr" => [], "mobileweb" => %{"en" => ""}, "sms" => %{"en" => []}},
+      #         "skip_logic" => nil,
+      #         "value" => ""
+      #       }
+      #     ],
+      #     "id" => "caad462b-afa4-4c9c-839d-c9e3e7ee4dc1",
+      #     "prompt" => %{
+      #       "en" => %{
+      #         "ivr" => %{"audio_source" => "tts", "text" => ""},
+      #         "mobileweb" => "",
+      #         "sms" => ""
+      #       }
+      #     },
+      #     "store" => "",
+      #     "title" => "",
+      #     "type" => "multiple-choice"
+      #   }
+      # ]
+      # clean = CleanI18n.clean(entity, ["en"], ".[].choices.[].responses.[]")
+
+      entity = %{"foo" => "bar"}
+
+      clean = CleanI18n.clean(entity, ["baz"], ".foo")
+
+      assert clean == %{"foo" => "bar"}
     end
   end
 end
