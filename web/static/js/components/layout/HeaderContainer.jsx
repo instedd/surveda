ed35063311d1a7b9b50551c265aa984e9f601058
aeatencio <aeatencio@gmail.com>
2021-06-18 18:06:54 -0300

    Fix the breadcrum

--- web/static/js/components/layout/HeaderContainer.jsx
+++ web/static/js/components/layout/HeaderContainer.jsx
@@ -16,8 +16,8 @@ class HeaderContainer extends Component {
   }
 
   render() {
-    const { tabs, logout, user, project, surveyFolder, panelSurveyId, panelSurvey } = this.props
-    const { projectId, surveyId, questionnaireId, folderId } = this.props.params
+    const { tabs, logout, user, project, folder, panelSurveyFromOccurrence } = this.props
+    const { projectId, surveyId, questionnaireId, folderId, panelSurveyId } = this.props.params
 
     let showProjectLink = true
     if (!project || (!surveyId && !questionnaireId && !folderId && !panelSurveyId)) {
@@ -35,7 +35,7 @@ class HeaderContainer extends Component {
     }
 
     return (
-      <Header tabs={tabs} logout={logout} user={user} showProjectLink={showProjectLink} showQuestionnairesLink={!!questionnaireId} project={project || null} surveyFolder={surveyFolder} panelSurvey={panelSurvey} />
+      <Header tabs={tabs} logout={logout} user={user} showProjectLink={showProjectLink} showQuestionnairesLink={!!questionnaireId} project={project || null} folder={folder} panelSurvey={panelSurveyFromOccurrence} />
     )
   }
 }
@@ -47,42 +47,78 @@ HeaderContainer.propTypes = {
   tabs: PropTypes.node,
   logout: PropTypes.func.isRequired,
   user: PropTypes.string.isRequired,
-  surveyFolder: PropTypes.object,
+  folder: PropTypes.object,
   panelSurveyId: PropTypes.number,
-  panelSurvey: PropTypes.object
+  panelSurveyFromOccurrence: PropTypes.object
 }
 
-const folder = (params, state) => {
-  const { surveyId, panelSurveyId } = params
-  let folderId = null
-  if (state.survey.data && state.survey.data.id == surveyId) {
-    folderId = state.survey.data.folderId
-  } else if (state.panelSurvey.data && state.panelSurvey.data.id == panelSurveyId) {
-    folderId = state.panelSurvey.data.folderId
+const getSurveyFromParams = (params, state) => {
+  const surveyId = params.surveyId && parseInt(params.surveyId)
+  if (!surveyId) return null
+  const survey = state.survey && state.survey.data
+  if (!survey) return null
+  if (survey.id == surveyId) {
+    return survey
+  } else {
+    return null
   }
-  const folder = state.folder && state.folder.folders && state.folder.folders[folderId]
-  return folder || null
 }
 
-const panelSurvey = (surveyId, state) => {
-  if (surveyId && state.survey && state.survey.data && state.survey.data.id == surveyId) {
-    const survey = state.survey.data
-    if (state.panelSurveys.items && state.panelSurveys.items[survey.panelSurveyOf]) {
-      return state.panelSurveys.items[survey.panelSurveyOf]
+const getPanelSurveyFromOccurrence = (survey, state) => {
+  if (survey) {
+    const panelSurvey = state.panelSurveys.items && state.panelSurveys.items[survey.panelSurveyId]
+    if (panelSurvey) {
+      return panelSurvey
     }
   }
   return null
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const { params } = ownProps
+const getPanelSurveyFromParams = (params, state) => {
   const panelSurveyId = params.panelSurveyId && parseInt(params.panelSurveyId)
-  const surveyId = params.surveyId && parseInt(params.surveyId)
+  if (!panelSurveyId) return null
+  const panelSurvey = state.panelSurvey && state.panelSurvey.data
+  if (!panelSurvey) return null
+  if (panelSurvey.id == panelSurveyId) {
+    return panelSurvey
+  } else {
+    return null
+  }
+}
+
+const getFolderFromSurveyOrPanelSurvey = (surveyOrPanelSurvey, state) => {
+  let folderId = null
+  if (surveyOrPanelSurvey) {
+    folderId = surveyOrPanelSurvey.folderId
+  }
+  const folder = state.folder && state.folder.folders && state.folder.folders[folderId]
+  return folder || null
+}
+
+const mapStateToProps = (state, ownProps) => {
+  /**
+   * These are the survey related breadcrum cases:
+    1. Project
+    2. Project -> Survey : <Project>
+    3. Project -> Panel Survey : <Project>
+    4. Project -> Panel Survey -> Occurrence : <Project | PanelSurvey>
+    5. Project -> Folder : <Project>
+    6. Project -> Folder -> Survey : <Project | Folder>
+    7. Project -> Folder -> Panel Survey : <Project | Folder>
+    8. Project -> Folder -> Panel Survey -> Occurrence : <Project | Folder | PanelSurvey>
+  */
+  const { params } = ownProps
+  const survey = getSurveyFromParams(params, state)
+  const panelSurvey = getPanelSurveyFromParams(params, state)
+  const panelSurveyFromOccurrence = getPanelSurveyFromOccurrence(survey, state)
+  // First evaluate the panel survey from occurrence (case 8)
+  // If the survey is evaluated before (case 6), the folder is missing for the case 8.
+  const surveyOrPanelSurvey = panelSurveyFromOccurrence || panelSurvey || survey
+  const folder = getFolderFromSurveyOrPanelSurvey(surveyOrPanelSurvey, state)
   return {
     project: state.project.data,
-    panelSurveyId,
-    surveyFolder: folder(params, state),
-    panelSurvey: panelSurvey(surveyId, state)
+    panelSurveyFromOccurrence: panelSurveyFromOccurrence,
+    folder
   }
 }
 
