427ffe65675b926f28a3729be61a4ba752b93fd5
manumoreira <mmoreira@manas.tech>
2021-07-28 15:23:24 -0300

    new wording for panel survey settings
    Enhanced wording for panel survey settings based on users feedback

--- web/static/js/components/surveys/SurveyWizardPanelSurveyStep.jsx
+++ web/static/js/components/surveys/SurveyWizardPanelSurveyStep.jsx
@@ -19,7 +19,7 @@ class SurveyWizardPanelSurveyStep extends Component {
         <div className='row'>
           <div className='col s12'>
             <h4>{t('Panel Survey')}</h4>
-            <p className='flow-text'>{t('This is an occurrence of a Panel Survey, it might inherit settings from previous occurrences (if any) and will be used as a template for the next occurrence.')}</p>
+            <p className='flow-text'>{t('This is a wave of a panel survey. The settings from previous waves will be used as a template for this wave. Any changes made to this wave\'s settings will serve as a template for future waves of this panel survey')}</p>
           </div>
         </div>
       </div>
