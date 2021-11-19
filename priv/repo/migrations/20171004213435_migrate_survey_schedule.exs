6d106661a663bacab2491fc0c49806c8db5b87cd
Julien Portalier <julien@portalier.com>
2021-11-19 11:16:02 +0100

    Fix: Ecto.Time in migration

--- priv/repo/migrations/20171004213435_migrate_survey_schedule.exs
+++ priv/repo/migrations/20171004213435_migrate_survey_schedule.exs
@@ -179,8 +179,8 @@ defmodule Ask.Repo.Migrations.MigrateSurveySchedule do
 
     schema "surveys" do
       field :schedule_day_of_week, DayOfWeek, default: DayOfWeek.never()
-      field :schedule_start_time, Ecto.Time
-      field :schedule_end_time, Ecto.Time
+      field :schedule_start_time, :time
+      field :schedule_end_time, :time
       field :timezone, :string
       field :schedule, Schedule, default: Schedule.default()
     end
