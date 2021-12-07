b6dcd0d18afd5f01208786d2f5bd3c0b96f802d8
Julien Portalier <julien@portalier.com>
2021-12-07 10:38:19 +0100

    Fix: cache-control header for AudioController and AudioDeliveryController
    The former did set the cache-control header properly, but the latter
    didn't, leading to poor performance with Twilio.

--- web/controllers/audio_controller.ex
+++ web/controllers/audio_controller.ex
@@ -34,8 +34,7 @@ defmodule Ask.AudioController do
     # can be downloaded, be careful to use a valid filename
     # (one that doesn't have commas or strange characters)
     conn
-    # Expires in one year ( recommended by RFC 2616: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.21 )
-    |> put_resp_header("cache-control", "max-age=31556926")
+    |> put_cache_headers()
     |> send_resp(200, audio.data)
   end
 
@@ -48,8 +47,8 @@ defmodule Ask.AudioController do
       System.cmd("/usr/bin/text2wave", ["-o", output, input], parallelism: true)
 
       conn
-      |> put_resp_header("cache-control", "max-age=31556926")
       |> put_resp_header("content-type", "audio/x-wav")
+      |> put_cache_headers()
       |> send_file(200, output)
     after
       File.rm(input)
