ee8cc3f3b4b3c3f3d7430afb702df32bacc16e18
Julien Portalier <julien@portalier.com>
2022-04-12 20:01:45 +0200

    Fix: errors in pre-commit hook

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
+
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+	against=HEAD
+else
+	# Initial commit: diff against an empty tree object
+	against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+# Redirect output to stderr.
+exec 1>&2
+
 NC='\033[0m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 
-STAGED_FILES="git diff-index --cached HEAD --name-only --diff-filter d"
-STAGED_ELIXIR_FILES=$($STAGED_FILES | egrep '\.(ex|exs|eex|heex)$')
-STAGED_ASSET_FILES=$($STAGED_FILES | egrep '\.(js|css|scss)$' | grep -v assets/vendor)
+if [ -z "$GIT_DIR" ]; then
+  # manual invocation: list all changed files
+  FILES="git diff-index $against --name-only --diff-filter d"
+else
+  # pre-commit hook: list cached files
+  FILES="git diff-index --cached $against --name-only --diff-filter d"
+fi
+
+ELIXIR_FILES=$($FILES | egrep '\.(ex|exs|eex|heex)$')
+ASSET_FILES=$($FILES | egrep '\.(js|jsx|css|scss)$' | grep assets/ | grep -v assets/vendor)
 
 MIX="docker-compose run --no-deps --rm app mix"
 YARN="docker-compose run --no-deps --rm webpack yarn"
@@ -19,15 +38,15 @@ report() {
   fi
 }
 
-if [ ! -z $STAGED_ELIXIR_FILES ]; then
-  echo -n "==== Running mix format... "
-  $MIX format ${STAGED_ELIXIR_FILES}
+if [ ! -z "$ELIXIR_FILES" ]; then
+  echo -n "==== Running mix format (check)... "
+  $MIX format --check-formatted ${ELIXIR_FILES}
   report $?
 fi
 
-if [ ! -z $STAGED_ASSET_FILES ]; then
-  echo -n "==== Running prettier... "
-  $YARN -s prettier -c ${STAGED_ASSET_FILES}
+if [ ! -z "$ASSET_FILES" ]; then
+  echo -n "==== Running prettier (check)... "
+  $YARN -s prettier -c --loglevel=warn ${ASSET_FILES}
   report $?
 fi
 
