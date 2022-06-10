b272f61a01aecd2dc44cfc92d2ca676a6ecc1c06
aeatencio <aatencio@manas.tech>
2022-06-10 13:32:31 -0300

    Avoid multiple implementations of broker channel
    See https://github.com/instedd/surveda/pull/2092#issuecomment-1151245069

--- lib/ask/runtime/ channel_broker.ex
+++ lib/ask/runtime/ channel_broker.ex
@@ -1,4 +1,11 @@
-defprotocol Ask.Runtime.ChannelBroker do
-  def setup(channel, respondent, token, not_before, not_after)
-  def ask(channel, respondent, token, prompts)
+alias Ask.Runtime.Channel
+
+defmodule Ask.Runtime.ChannelBroker do
+  def setup(channel, respondent, token, not_before, not_after) do
+    Channel.setup(channel, respondent, token, not_before, not_after)
+  end
+
+  def ask(channel, respondent, token, reply) do
+    Channel.ask(channel, respondent, token, reply)
+  end
 end
