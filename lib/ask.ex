4731c3a38482cd3f57e6c9f9003affd3a4210415
aeatencio <aatencio@manas.tech>
2022-06-28 13:14:10 -0300

    Fix the survey canceller supervisor

--- lib/ask.ex
+++ lib/ask.ex
@@ -19,6 +19,7 @@ defmodule Ask do
       supervisor(Ask.MetricsEndpoint, []),
       supervisor(Registry, [:unique, :channel_broker_registry]),
       supervisor(Ask.Runtime.ChannelBrokerSupervisor, []),
+      supervisor(Ask.Runtime.SurveyCancellerSupervisor, []),
       {Mutex, name: Ask.Mutex}
       # Start your own worker by calling: Ask.Worker.start_link(arg1, arg2, arg3)
       # worker(Ask.Worker, [arg1, arg2, arg3]),
@@ -46,27 +47,7 @@ defmodule Ask do
     opts = [strategy: :one_for_one, name: Ask.Supervisor]
     {:ok, _} = Logger.add_backend(Sentry.LoggerBackend)
 
-    supervisor_result = Supervisor.start_link(children, opts)
-
-    # survey_canceller_children =
-    #   if Mix.env() != :test && !IEx.started?() do
-    #     # Start cancelling with survey_id = nil to check all surveys that must be cancelled
-    #     survey_canceller = Ask.SurveyCanceller.start_cancelling(nil)
-
-    #     case survey_canceller do
-    #       :ignore ->
-    #         nil
-
-    #       %Ask.SurveyCanceller{processes: _, consumers_pids: _} ->
-    #         survey_canceller.processes
-    #     end
-    #   end
-
-    # if Mix.env() != :test && !IEx.started?() && survey_canceller_children do
-    #   Supervisor.start_link(survey_canceller_children, strategy: :rest_for_one)
-    # end
-
-    supervisor_result
+    Supervisor.start_link(children, opts)
   end
 
   # Tell Phoenix to update the endpoint configuration
