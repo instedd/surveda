21d95c8b5b2b25a4ac6394e90700840f99150ce3
Julien Portalier <julien@portalier.com>
2023-04-07 20:10:48 +0200

    Test VerboiceChannel.callback/3 notifies ChannelBroker (or not)

--- test/ask/runtime/verboice_channel_test.exs
+++ test/ask/runtime/verboice_channel_test.exs
@@ -14,9 +14,11 @@ defmodule Ask.Runtime.VerboiceChannelTest do
     SurveyBroker,
     ChannelStatusServer,
     SurveyStub,
+    ChannelBroker,
     ChannelBrokerAgent
   }
 
+  import Mock
   require Ask.Runtime.ReplyHelper
 
   @survey %{schedule: Ask.Schedule.always()}
@@ -122,21 +124,28 @@ defmodule Ask.Runtime.VerboiceChannelTest do
            end}
         )
 
-        digits =
-          case flow_message do
-            :answer -> nil
-            {:reply, digits} -> digits
+        with_mock ChannelBroker, [callback_received: fn(_, _, _, _) -> :ok end] do
+          digits =
+            case flow_message do
+              :answer -> nil
+              {:reply, digits} -> digits
+            end
+
+          conn = VerboiceChannel.callback(conn, %{"respondent" => respondent_id, "Digits" => digits}, SurveyStub)
+
+          case elem(step, 0) do
+            :end ->
+              # reports completed call to channel broker
+              match_respondent = :meck.is(fn %Ask.Respondent{id: id} -> assert id == respondent.id end)
+              assert_called ChannelBroker.callback_received(0, match_respondent, "completed", "verboice")
+            _ ->
+              # won't report in-progress calls to channel broker
+              assert_not_called ChannelBroker.callback_received(:_, :_, :_, :_)
           end
 
-        conn =
-          VerboiceChannel.callback(
-            conn,
-            %{"respondent" => respondent_id, "Digits" => digits},
-            SurveyStub
-          )
-
-        response_twiml = response(conn, 200) |> trim_xml
-        assert response_twiml =~ twiml
+          response_twiml = response(conn, 200) |> trim_xml
+          assert response_twiml =~ twiml
+        end
     end)
   end
 
