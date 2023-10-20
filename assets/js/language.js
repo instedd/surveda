1b192046e300e8525936ce212b90be3ff6ec8c9e
Matias Garcia Isaia <mgarcia@manas.tech>
2023-10-20 04:18:21 -0300

    Override language names in UI when enabled

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,18 +1,30 @@
 // @flow
 import iso6393 from "iso-639-3"
+import { config } from "./config"
+
+const languages = (() => {
+  let langs = iso6393
+  for (const [code, name] of Object.entries(config.custom_language_names)) {
+    let lang
+    if (lang = langs.find((l) => l.iso6393 == code)) {
+      lang.name = name
+    }
+  }
+  return langs
+})()
 
 export function nameToCode(name: string): ?string {
-  let match = iso6393.find((l) => l.name == name)
+  let match = languages.find((l) => l.name == name)
   return match ? idForLanguage(match) : null
 }
 
 export const idForLanguage = (lang) => (lang.iso6391 ? lang.iso6391 : lang.iso6393)
 
 export function livingLanguages() {
-  return iso6393.filter((lang) => lang.type == "living")
+  return languages.filter((lang) => lang.type == "living")
 }
 
 export function codeToName(code: string): ?string {
-  let match = iso6393.find((l) => l.iso6391 == code || l.iso6393 == code)
+  let match = languages.find((l) => l.iso6391 == code || l.iso6393 == code)
   return match ? match.name : null
 }
