1b52782934dc2aa1ba7329e12a929128504730fa
Ana Perez Ghiglia <aghiglia@manas.tech>
2024-04-22 16:09:24 -0300

    Fix migration

--- priv/repo/migrations/20240422175453_fix_respondent_stats_trigger.exs
+++ priv/repo/migrations/20240422175453_fix_respondent_stats_trigger.exs
@@ -1,91 +1,89 @@
 defmodule Ask.Repo.Migrations.FixRespondentStatsTrigger do
   use Ecto.Migration
+  alias Ask.Repo
 
   def up do
-    Repo.transaction(fn ->
-      Repo.query!(
-        """
-        DROP TRIGGER respondents_upd;
-        """
-      )
+    Repo.query!(
+      """
+      DROP TRIGGER respondents_upd;
+      """
+    )
 
-      Repo.query!(
-        Repo.query!(
-          """
-          CREATE TRIGGER respondents_upd
-          AFTER UPDATE ON respondents
-          FOR EACH ROW
-          BEGIN
-    
-            # This is a hack for using the `select for update`
-            # so we can lock all the records that will get updated 
-            # by this trigger before actually modifying them
-            # so we prevent this trigger to generate deadlocks
-            # see #1744
-            DECLARE temp_stats INT;
-    
-            SELECT count(*) 
-            INTO temp_stats
-            FROM respondent_stats
-            WHERE (survey_id = OLD.survey_id
-                      AND questionnaire_id = IFNULL(OLD.questionnaire_id, 0)
-                      AND state = OLD.state
-                      AND disposition = OLD.disposition
-                      AND quota_bucket_id = IFNULL(OLD.quota_bucket_id, 0)
-                      AND mode = IFNULL(OLD.mode, ''))
-            OR (survey_id = NEW.survey_id
-                      AND questionnaire_id = IFNULL(NEW.questionnaire_id, 0)
-                      AND state = NEW.state
-                      AND disposition = NEW.disposition
-                      AND quota_bucket_id = IFNULL(NEW.quota_bucket_id, 0)
-                      AND mode = IFNULL(NEW.mode, ''))
-            FOR UPDATE;
-    
-            UPDATE respondent_stats
-               SET `count` = `count` - 1
-             WHERE survey_id = OLD.survey_id
-               AND questionnaire_id = IFNULL(OLD.questionnaire_id, 0)
-               AND state = OLD.state
-               AND disposition = OLD.disposition
-               AND quota_bucket_id = IFNULL(OLD.quota_bucket_id, 0)
-               AND mode = IFNULL(OLD.mode, '')
-            ;
+    Repo.query!(
+      """
+      CREATE TRIGGER respondents_upd
+      AFTER UPDATE ON respondents
+      FOR EACH ROW
+      BEGIN
 
-            INSERT INTO respondent_stats(survey_id, questionnaire_id, state, disposition, quota_bucket_id, mode, `count`)
-            VALUES (NEW.survey_id, IFNULL(NEW.questionnaire_id, 0), NEW.state, NEW.disposition, IFNULL(NEW.quota_bucket_id, 0), IFNULL(NEW.mode, ''), 1)
-            ON DUPLICATE KEY UPDATE `count` = `count` + 1
-            ;
+        # This is a hack for using the `select for update`
+        # so we can lock all the records that will get updated 
+        # by this trigger before actually modifying them
+        # so we prevent this trigger to generate deadlocks
+        # see #1744
+        DECLARE temp_stats INT;
+
+        SELECT count(*) 
+        INTO temp_stats
+        FROM respondent_stats
+        WHERE (survey_id = OLD.survey_id
+                  AND questionnaire_id = IFNULL(OLD.questionnaire_id, 0)
+                  AND state = OLD.state
+                  AND disposition = OLD.disposition
+                  AND quota_bucket_id = IFNULL(OLD.quota_bucket_id, 0)
+                  AND mode = IFNULL(OLD.mode, ''))
+        OR (survey_id = NEW.survey_id
+                  AND questionnaire_id = IFNULL(NEW.questionnaire_id, 0)
+                  AND state = NEW.state
+                  AND disposition = NEW.disposition
+                  AND quota_bucket_id = IFNULL(NEW.quota_bucket_id, 0)
+                  AND mode = IFNULL(NEW.mode, ''))
+        FOR UPDATE;
+
+        UPDATE respondent_stats
+            SET `count` = `count` - 1
+          WHERE survey_id = OLD.survey_id
+            AND questionnaire_id = IFNULL(OLD.questionnaire_id, 0)
+            AND state = OLD.state
+            AND disposition = OLD.disposition
+            AND quota_bucket_id = IFNULL(OLD.quota_bucket_id, 0)
+            AND mode = IFNULL(OLD.mode, '')
+        ;
+
+        INSERT INTO respondent_stats(survey_id, questionnaire_id, state, disposition, quota_bucket_id, mode, `count`)
+        VALUES (NEW.survey_id, IFNULL(NEW.questionnaire_id, 0), NEW.state, NEW.disposition, IFNULL(NEW.quota_bucket_id, 0), IFNULL(NEW.mode, ''), 1)
+        ON DUPLICATE KEY UPDATE `count` = `count` + 1
+        ;
+
+      END;
+      """
+      ) 
     
-          END;
-          """
-        )
-      )
-    end)
   end
 
   def down do
     # Re-create the trigger as it was before
-    Repo.transaction(fn ->
-      Repo.query!(
-        """
-        DROP TRIGGER respondents_upd;
-        """
-      )
+    Repo.query!(
+      """
+      DROP TRIGGER respondents_upd;
+      """
+    )
 
-      Repo.query!("""
+    Repo.query!(
+      """
       CREATE TRIGGER respondents_upd
       AFTER UPDATE ON respondents
       FOR EACH ROW
       BEGIN
 
         UPDATE respondent_stats
-           SET `count` = `count` - 1
-         WHERE survey_id = OLD.survey_id
-           AND questionnaire_id = IFNULL(OLD.questionnaire_id, 0)
-           AND state = OLD.state
-           AND disposition = OLD.disposition
-           AND quota_bucket_id = IFNULL(OLD.quota_bucket_id, 0)
-           AND mode = IFNULL(OLD.mode, '')
+          SET `count` = `count` - 1
+        WHERE survey_id = OLD.survey_id
+          AND questionnaire_id = IFNULL(OLD.questionnaire_id, 0)
+          AND state = OLD.state
+          AND disposition = OLD.disposition
+          AND quota_bucket_id = IFNULL(OLD.quota_bucket_id, 0)
+          AND mode = IFNULL(OLD.mode, '')
         ;
 
         INSERT INTO respondent_stats(survey_id, questionnaire_id, state, disposition, quota_bucket_id, mode, `count`)
@@ -95,6 +93,5 @@ defmodule Ask.Repo.Migrations.FixRespondentStatsTrigger do
 
       END;
       """)
-    end)
   end
 end
