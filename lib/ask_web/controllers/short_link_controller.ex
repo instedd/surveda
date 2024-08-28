55e315b5b51d3f00c77c8fb32b52aedac445483d
Ismael Bejarano <ibejarano@manas.tech>
2024-08-28 10:35:41 -0300

    Fix CI failure

--- lib/ask_web/controllers/short_link_controller.ex
+++ lib/ask_web/controllers/short_link_controller.ex
@@ -9,35 +9,34 @@ defmodule AskWeb.ShortLinkController do
       ShortLink
       |> Repo.get_by(hash: hash)
 
-    unless link do
+    if link do
+      conn =
+        conn
+        |> assign(:skip_auth, true)
+
+      {path, query_string} =
+        case :binary.split(link.target, "?", [:global]) do
+          [path, query_string] -> {path, query_string}
+          [path] -> {path, ""}
+        end
+
+      conn = %{
+        conn
+        | request_path: path,
+          path_info: split_path(path),
+          params: %{},
+          path_params: %{},
+          private: %{},
+          req_headers: [],
+          query_string: query_string,
+          query_params: %Plug.Conn.Unfetched{aspect: :query_params}
+      }
+
+      AskWeb.Endpoint.call(conn, [])
+    else
       conn
-      |> put_status(:not_found)
       |> send_resp(:not_found, "")
     end
-
-    conn =
-      conn
-      |> assign(:skip_auth, true)
-
-    {path, query_string} =
-      case :binary.split(link.target, "?", [:global]) do
-        [path, query_string] -> {path, query_string}
-        [path] -> {path, ""}
-      end
-
-    conn = %{
-      conn
-      | request_path: path,
-        path_info: split_path(path),
-        params: %{},
-        path_params: %{},
-        private: %{},
-        req_headers: [],
-        query_string: query_string,
-        query_params: %Plug.Conn.Unfetched{aspect: :query_params}
-    }
-
-    AskWeb.Endpoint.call(conn, [])
   end
 
   # Copied from Plug.Adapters.Cowboy.Conn
