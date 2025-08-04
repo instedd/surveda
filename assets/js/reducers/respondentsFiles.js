4385cc99f0bc22054b89f54d89ae65750d4ea294
Matías García Isaía <mgarcia@manas.tech>
2025-08-04 16:47:24 -0300

    Instantly mark file as being generated on UI
    See #2385

--- assets/js/reducers/respondentsFiles.js
+++ assets/js/reducers/respondentsFiles.js
@@ -2,6 +2,19 @@ import * as actions from "../actions/survey"
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case actions.GENERATING_FILE:
+      return {
+        ...state,
+        files: {
+          ...(state.files || {}),
+          [action.file]: {
+            ...(state.files || {})[action.file],
+            file_type: action.file,
+            creating: true,
+            created_at: null
+          }
+        }
+      }
     case actions.FETCHING_FILES_STATUS:
       if (action.surveyId != state.surveyId) {
         return {
